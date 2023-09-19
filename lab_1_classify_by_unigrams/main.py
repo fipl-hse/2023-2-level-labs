c102bb9bca6aab7a6785b357dd8df893736c0058
AnPruch <annet.zdesnet@yandex.ru>
2023-09-20 01:51:51 +0300

    I've made Steps 1-9 so far, corresponding tests run perfectly, but I didn't understand how you want me to import functions to 'start.py', because the file name 'main' triggers the function name.

--- lab_1_classify_by_unigrams/main.py
+++ lab_1_classify_by_unigrams/main.py
@@ -2,6 +2,7 @@
 Lab 1
 Language detection
 """
+import json
 
 
 def tokenize(text: str) -> list[str] | None:
@@ -11,6 +12,12 @@ def tokenize(text: str) -> list[str] | None:
     :param text: a text
     :return: a list of lower-cased tokens without punctuation
     """
+    if not isinstance(text, str):
+        return None
+
+    tokens = [character for character in text.lower() if character.isalpha()]
+
+    return tokens
 
 
 def calculate_frequencies(tokens: list[str] | None) -> dict[str, float] | None:
@@ -19,6 +26,16 @@ def calculate_frequencies(tokens: list[str] | None) -> dict[str, float] | None:
     :param tokens: a list of tokens
     :return: a dictionary with frequencies
     """
+    if not isinstance(tokens, list) or not all(isinstance(char, str) for char in tokens):
+        return None
+
+    text_freq = {}
+    for character in tokens:
+        if character not in text_freq:
+            text_freq[character] = 0
+        text_freq[character] += 1/len(tokens)
+
+    return text_freq
 
 
 def create_language_profile(language: str, text: str) -> dict[str, str | dict[str, float]] | None:
@@ -28,6 +45,13 @@ def create_language_profile(language: str, text: str) -> dict[str, str | dict[st
     :param text: a text
     :return: a dictionary with two keys – name, freq
     """
+    if not isinstance(language, str) or not isinstance(text, str):
+        return None
+
+    tokens = tokenize(text)
+    text_freq = calculate_frequencies(tokens)
+
+    return {'name': language, 'freq': text_freq}
 
 
 def calculate_mse(predicted: list, actual: list) -> float | None:
@@ -37,6 +61,15 @@ def calculate_mse(predicted: list, actual: list) -> float | None:
     :param actual: a list of actual values
     :return: the score
     """
+    if not isinstance(predicted, list) or not isinstance(actual, list) or len(predicted) != len(actual):
+        return None
+
+    MSE = 0
+    for index in range(len(predicted)):
+        MSE += (predicted[index] - actual[index])**2
+
+    MSE /= len(predicted)
+    return MSE
 
 
 def compare_profiles(
@@ -49,6 +82,26 @@ def compare_profiles(
     :param profile_to_compare: a dictionary of a profile to compare the unknown profile to
     :return: the distance between the profiles
     """
+    if not isinstance(unknown_profile, dict) or not isinstance(profile_to_compare, dict) \
+            or ('name' or 'freq') not in unknown_profile or ('name' or 'freq') not in profile_to_compare:
+        return None
+
+    mutual_profile = {}
+    mutual_profile.update(profile_to_compare['freq'])
+    mutual_profile.update(unknown_profile['freq'])
+    mutual_characters = [[], []]
+    for character in mutual_profile:
+        mutual_characters[0].append(0)
+        mutual_characters[1].append(0)
+
+        if character in unknown_profile['freq']:
+            mutual_characters[0][-1] = unknown_profile['freq'][character]
+        if character in profile_to_compare['freq']:
+            mutual_characters[1][-1] = profile_to_compare['freq'][character]
+
+    if len(mutual_characters[0]) == len(mutual_characters[1]) == 0:
+        return 1.
+    return calculate_mse(mutual_characters[0], mutual_characters[1])
 
 
 def detect_language(
@@ -63,6 +116,18 @@ def detect_language(
     :param profile_2: a dictionary of a known profile
     :return: a language
     """
+    if not all(isinstance(one_profile, dict) for one_profile in [unknown_profile, profile_1, profile_2]):
+        return None
+
+    MSE_1 = compare_profiles(unknown_profile, profile_1)
+    MSE_2 = compare_profiles(unknown_profile, profile_2)
+
+    if MSE_1 == MSE_2:
+        return sorted([profile_1['name'], profile_2['name']])[0]
+    elif MSE_1 < MSE_2:
+        return profile_1['name']
+    else:
+        return profile_2['name']
 
 
 def load_profile(path_to_file: str) -> dict | None:
@@ -71,6 +136,11 @@ def load_profile(path_to_file: str) -> dict | None:
     :param path_to_file: a path to the language profile
     :return: a dictionary with at least two keys – name, freq
     """
+    if not isinstance(path_to_file, str):
+        return None
+
+    with open(path_to_file, 'r', encoding='utf-8') as file:
+        return json.load(file)
 
 
 def preprocess_profile(profile: dict) -> dict[str, str | dict] | None:
@@ -80,6 +150,20 @@ def preprocess_profile(profile: dict) -> dict[str, str | dict] | None:
     :return: a dict with a lower-cased loaded profile
     with relative frequencies without unnecessary ngrams
     """
+    if not isinstance(profile, dict) or 'name' not in profile \
+            or 'n_words' not in profile or 'freq' not in profile:
+        return None
+
+    perfect_profile = {'name': profile['name'],
+                       'freq': {}}
+
+    for sequence in profile['freq']:
+        if sequence.lower() in perfect_profile['freq']:
+            perfect_profile['freq'][sequence.lower()] += profile['freq'][sequence] / profile['n_words'][0]
+        elif len(sequence) == 1:
+            perfect_profile['freq'][sequence.lower()] = profile['freq'][sequence] / profile['n_words'][0]
+
+    return perfect_profile
 
 
 def collect_profiles(paths_to_profiles: list) -> list[dict[str, str | dict[str, float]]] | None:
@@ -88,6 +172,18 @@ def collect_profiles(paths_to_profiles: list) -> list[dict[str, str | dict[str, 
     :paths_to_profiles: a list of strings to the profiles
     :return: a list of loaded profiles
     """
+    if not isinstance(paths_to_profiles, list):
+        return None
+
+    loaded_profiles = []
+    for path in paths_to_profiles:
+        loaded_profiles.append(load_profile(path))
+
+    preprocessed_profiles = []
+    for profile in loaded_profiles:
+        preprocessed_profiles.append(preprocess_profile(profile))
+
+    return preprocessed_profiles
 
 
 def detect_language_advanced(unknown_profile: dict[str, str | dict[str, float]],
--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -1,12 +1,29 @@
 """
 Language detection starter
 """
-
+# from main import load_profile
+'''
+I couldn't figure out how to import functions as it was implied in Step 0. 
+It seems strange because both the function below and the main file share the same name
+ 
+'''
 
 def main() -> None:
     """
     Launches an implementation
     """
+    profiles_paths = ['assets/profiles/de.json',
+                      'assets/profiles/en.json',
+                      'assets/profiles/es.json',
+                      'assets/profiles/fr.json',
+                      'assets/profiles/it.json',
+                      'assets/profiles/ru.json',
+                      'assets/profiles/tr.json',
+                      ]
+    # language_profiles = []
+    # for path in profiles_paths:
+    #     language_profiles.append(load_profile(path))
+
     with open("assets/texts/en.txt", "r", encoding="utf-8") as file_to_read_en:
         en_text = file_to_read_en.read()
     with open("assets/texts/de.txt", "r", encoding="utf-8") as file_to_read_de:
