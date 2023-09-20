aca5a90fd347d7a89f9953012ca85a394afd1b06
solonarr <zakker090@gmail.com>
2023-09-20 15:59:32 +0300

    mark 6

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -1,7 +1,8 @@
+from lab_1_classify_by_unigrams.main import create_language_profile
+
 """
 Language detection starter
 """
-from lab_1_classify_by_unigrams.main import create_language_profile
 
 
 def main() -> None:
