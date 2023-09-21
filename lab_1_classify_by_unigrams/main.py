cefb16096f062c7008ac42ac804fb05b74cd8ab3
AlisaKarpova <aliskakarpowa@yandex.ru>
2023-09-21 19:36:00 +0300

    main.py

--- lab_1_classify_by_unigrams/main.py
+++ lab_1_classify_by_unigrams/main.py
@@ -4,7 +4,13 @@ Language detection
 """
 
 
-def tokenize(text):
+def tokenize(text) -> list[str]:
+    """
+        Splits a text into tokens, converts the tokens into lowercase,
+        removes punctuation, digits and other symbols
+        :param text: a text
+        :return: a list of lower-cased tokens without punctuation
+        """
     text = text.lower()
     for i in text:
         if not i.isalnum() and i != ' ':
@@ -22,6 +28,11 @@ tokens = tokenize(text)
 
 
 def calculate_frequencies(tokens):
+    """
+       Calculates frequencies of given tokens
+       :param tokens: a list of tokens
+       :return: a dictionary with frequencies
+       """
     dictionary = {}
     explanation = {}
     length = len(tokens)
@@ -32,17 +43,32 @@ def calculate_frequencies(tokens):
 
 
 def create_language_profile(language: str, text: str) -> dict[str, str | dict[str, float]] | None:
-
+    """
+      Creates a language profile
+      :param language: a language
+      :param text: a text
+      :return: a dictionary with two keys – name, freq
+      """
 
 def calculate_mse(predicted: list, actual: list) -> float | None:
-
+    """
+       Calculates mean squared error between predicted and actual values
+       :param predicted: a list of predicted values
+       :param actual: a list of actual values
+       :return: the score
+       """
 
 
 def compare_profiles(
         unknown_profile: dict[str, str | dict[str, float]],
         profile_to_compare: dict[str, str | dict[str, float]]
 ) -> float | None:
-
+    """
+       Compares profiles and calculates the distance using symbols
+       :param unknown_profile: a dictionary of an unknown profile
+       :param profile_to_compare: a dictionary of a profile to compare the unknown profile to
+       :return: the distance between the profiles
+       """
 
 
 def detect_language(
@@ -50,25 +76,52 @@ def detect_language(
         profile_1: dict[str, str | dict[str, float]],
         profile_2: dict[str, str | dict[str, float]],
 ) -> str | None:
-
+    """
+        Detects the language of an unknown profile
+        :param unknown_profile: a dictionary of a profile to determine the language of
+        :param profile_1: a dictionary of a known profile
+        :param profile_2: a dictionary of a known profile
+        :return: a language
+        """
 
 
 def load_profile(path_to_file: str) -> dict | None:
-
+    """
+           Loads a language profile
+           :param path_to_file: a path to the language profile
+           :return: a dictionary with at least two keys – name, freq
+           """
 
 
 def preprocess_profile(profile: dict) -> dict[str, str | dict] | None:
-
+    """
+           Preprocesses profile for a loaded language
+           :param profile: a loaded profile
+           :return: a dict with a lower-cased loaded profile
+           with relative frequencies without unnecessary ngrams
+           """
 
 
 def collect_profiles(paths_to_profiles: list) -> list[dict[str, str | dict[str, float]]] | None:
-
+    """
+           Collects profiles for a given path
+           :paths_to_profiles: a list of strings to the profiles
+           :return: a list of loaded profiles
+           """
 
 
 def detect_language_advanced(unknown_profile: dict[str, str | dict[str, float]],
                              known_profiles: list) -> list | None:
-
+    """
+           Detects the language of an unknown profile
+           :param unknown_profile: a dictionary of a profile to determine the language of
+           :param known_profiles: a list of known profiles
+           :return: a sorted list of tuples containing a language and a distance
+           """
 
 
 def print_report(detections: list[list[str | float]]) -> None:
-
+    """
+        Prints report for detection of language
+        :param detections: a list with distances for each available language
+        """
