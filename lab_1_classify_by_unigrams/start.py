dffe3579bf0c63db1f95e4051e7a5354a71f1d75
Arina Savina <arinasavina330@gmail.com>
2023-09-21 16:17:04 +0300

    Laboratory work #1, Savina Arina - 22FPL1

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -4,17 +4,7 @@ Language detection starter
 
 
 def main() -> None:
-    """
-    Launches an implementation
-    """
-    with open("assets/texts/en.txt", "r", encoding="utf-8") as file_to_read_en:
-        en_text = file_to_read_en.read()
-    with open("assets/texts/de.txt", "r", encoding="utf-8") as file_to_read_de:
-        de_text = file_to_read_de.read()
-    with open("assets/texts/unknown.txt", "r", encoding="utf-8") as file_to_read_unk:
-        unknown_text = file_to_read_unk.read()
-    result = None
-    assert result, "Detection result is None"
+    pass
 
 
 if __name__ == "__main__":
