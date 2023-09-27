c8fd66c14436c5982f621c0b3bce1300f1e5cc40
solonarr <zakker090@gmail.com>
2023-09-27 11:58:05 +0300

    fight with import style

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -1,4 +1,8 @@
-from lab_1_classify_by_unigrams.main import create_language_profile, detect_language
+"""
+Language detection starter
+"""
+from lab_1_classify_by_unigrams.main import create_language_profile
+from lab_1_classify_by_unigrams.main import detect_language
 
 
 def main() -> None:
