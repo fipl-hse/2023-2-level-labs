906cb5dd99a56906e529a988928f258d824615fd
AlisaKarpova <aliskakarpowa@yandex.ru>
2023-09-28 20:00:06 +0300

    main.py

--- lab_1_classify_by_unigrams/main.py
+++ lab_1_classify_by_unigrams/main.py
@@ -6,11 +6,11 @@ Language detection
 
 def tokenize(text: str) -> list[str] | None:
     """
-        Splits a text into tokens, converts the tokens into lowercase,
-        removes punctuation, digits and other symbols
-        :param text: a text
-        :return: a list of lower-cased tokens without punctuation
-        """
+    Splits a text into tokens, converts the tokens into lowercase,
+    removes punctuation, digits and other symbols
+    :param text: a text
+    :return: a list of lower-cased tokens without punctuation
+    """
     if type(text) != str:
         return None
     tokens = []
@@ -22,10 +22,10 @@ def tokenize(text: str) -> list[str] | None:
 
 def calculate_frequencies(tokens: list[str] | None) -> dict[str, float] | None:
     """
-       Calculates frequencies of given tokens
-       :param tokens: a list of tokens
-       :return: a dictionary with frequencies
-       """
+    Calculates frequencies of given tokens
+    :param tokens: a list of tokens
+    :return: a dictionary with frequencies
+    """
     if type(tokens) != list:
         return None
     for i in tokens:
@@ -42,11 +42,11 @@ def calculate_frequencies(tokens: list[str] | None) -> dict[str, float] | None:
 
 def create_language_profile(language: str, text: str) -> dict[str, str | dict[str, float]] | None:
     """
-      Creates a language profile
-      :param language: a language
-      :param text: a text
-      :return: a dictionary with two keys – name, freq
-      """
+    Creates a language profile
+    :param language: a language
+    :param text: a text
+    :return: a dictionary with two keys – name, freq
+    """
     if type(language) != str or type(text) != str:
         return None
     new_dictionary = {}
@@ -57,11 +57,11 @@ def create_language_profile(language: str, text: str) -> dict[str, str | dict[st
 
 def calculate_mse(predicted: list, actual: list) -> float | None:
     """
-       Calculates mean squared error between predicted and actual values
-       :param predicted: a list of predicted values
-       :param actual: a list of actual values
-       :return: the score
-       """
+    Calculates mean squared error between predicted and actual values
+    :param predicted: a list of predicted values
+    :param actual: a list of actual values
+    :return: the score
+    """
     if type(predicted) != list or type(actual) != list or len(predicted) != len(actual):
         return None
     cubs = 0
@@ -78,11 +78,11 @@ def compare_profiles(
         profile_to_compare: dict[str, str | dict[str, float]]
 ) -> float | None:
     """
-       Compares profiles and calculates the distance using symbols
-       :param unknown_profile: a dictionary of an unknown profile
-       :param profile_to_compare: a dictionary of a profile to compare the unknown profile to
-       :return: the distance between the profiles
-       """
+     Compares profiles and calculates the distance using symbols
+    :param unknown_profile: a dictionary of an unknown profile
+    :param profile_to_compare: a dictionary of a profile to compare the unknown profile to
+    :return: the distance between the profiles
+    """
     if type(unknown_profile) != dict or type(profile_to_compare) != dict:
         return None
     key1 = 'name'
@@ -119,12 +119,12 @@ def detect_language(
         profile_2: dict[str, str | dict[str, float]],
 ) -> str | None:
     """
-        Detects the language of an unknown profile
-        :param unknown_profile: a dictionary of a profile to determine the language of
-        :param profile_1: a dictionary of a known profile
-        :param profile_2: a dictionary of a known profile
-        :return: a language
-        """
+    Detects the language of an unknown profile
+    :param unknown_profile: a dictionary of a profile to determine the language of
+    :param profile_1: a dictionary of a known profile
+    :param profile_2: a dictionary of a known profile
+    :return: a language
+    """
     if type(unknown_profile) != dict or type(profile_1) != dict or type(profile_2) != dict:
         return None
     if compare_profiles(unknown_profile, profile_1) < compare_profiles(unknown_profile, profile_2):
@@ -135,41 +135,41 @@ def detect_language(
 
 def load_profile(path_to_file: str) -> dict | None:
     """
-           Loads a language profile
-           :param path_to_file: a path to the language profile
-           :return: a dictionary with at least two keys – name, freq
-           """
+    Loads a language profile
+    :param path_to_file: a path to the language profile
+    :return: a dictionary with at least two keys – name, freq
+     """
 
 
 def preprocess_profile(profile: dict) -> dict[str, str | dict] | None:
     """
-           Preprocesses profile for a loaded language
-           :param profile: a loaded profile
-           :return: a dict with a lower-cased loaded profile
-           with relative frequencies without unnecessary ngrams
-           """
+    Preprocesses profile for a loaded language
+    :param profile: a loaded profile
+    :return: a dict with a lower-cased loaded profile
+    with relative frequencies without unnecessary ngrams
+    """
 
 
 def collect_profiles(paths_to_profiles: list) -> list[dict[str, str | dict[str, float]]] | None:
     """
-           Collects profiles for a given path
-           :paths_to_profiles: a list of strings to the profiles
-           :return: a list of loaded profiles
-           """
+    Collects profiles for a given path
+    :paths_to_profiles: a list of strings to the profiles
+    :return: a list of loaded profiles
+    """
 
 
 def detect_language_advanced(unknown_profile: dict[str, str | dict[str, float]],
                              known_profiles: list) -> list | None:
     """
-           Detects the language of an unknown profile
-           :param unknown_profile: a dictionary of a profile to determine the language of
-           :param known_profiles: a list of known profiles
-           :return: a sorted list of tuples containing a language and a distance
-           """
+    Detects the language of an unknown profile
+    :param unknown_profile: a dictionary of a profile to determine the language of
+    :param known_profiles: a list of known profiles
+    :return: a sorted list of tuples containing a language and a distance
+    """
 
 
 def print_report(detections: list[tuple[str, float]]) -> None:
     """
-        Prints report for detection of language
-        :param detections: a list with distances for each available language
-        """
+    Prints report for detection of language
+    :param detections: a list with distances for each available language
+    """
