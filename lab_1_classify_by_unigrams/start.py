86c1999d2cff6f2cc8076f1126f06d8de6b622a4
elena <eiermal@edu.hse.ru>
2023-09-29 09:43:54 +0300

    deleted

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -2,10 +2,8 @@
 Language detection starter
 """
 
-from lab_1_classify_by_unigrams.main import (
-    compare_profiles,
-    create_language_profile
-)
+from lab_1_classify_by_unigrams.main import (compare_profiles,
+                                             create_language_profile)
 
 
 def main() -> None:
