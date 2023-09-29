bf3b5219ecacfc3439aee424ff968ad794eded4e
elena <eiermal@edu.hse.ru>
2023-09-29 12:26:54 +0300

    start

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -16,13 +16,14 @@ def main() -> None:
         de_text = file_to_read_de.read()
     with open("assets/texts/unknown.txt", "r", encoding="utf-8") as file_to_read_unk:
         unknown_text = file_to_read_unk.read()
-    result = None
-    assert result, "Detection result is None"
-
     en_profile = create_language_profile('en', en_text)
     de_profile = create_language_profile('de', de_text)
-    unknown_profile = create_language_profile('unk', unknown_text)
-    detect_language(unknown_profile, en_profile, de_profile)
+    unknown_profile = create_language_profile('unknown', unknown_text)
+    result = detect_language(unknown_profile, en_profile, de_profile)
+    print(result)
+    assert result, "Detection result is None"
+
+
 
 
 if __name__ == "__main__":
