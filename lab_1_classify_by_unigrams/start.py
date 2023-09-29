3443af903314a69ef8d1052f2c4c38446d68f152
logunova-anastasia <lgnvssst@gmail.com>
2023-09-29 10:08:11 +0300

    add 'hi'

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -1,6 +1,7 @@
 """
 Language detection starter
 """
+from main import create_language_profile
 
 
 def main() -> None:
@@ -13,9 +14,13 @@ def main() -> None:
         de_text = file_to_read_de.read()
     with open("assets/texts/unknown.txt", "r", encoding="utf-8") as file_to_read_unk:
         unknown_text = file_to_read_unk.read()
+    en_profile = create_language_profile('en', en_text)
+    de_profile = create_language_profile('de', de_text)
+    unknown_profile = create_language_profile('unknown', unknown_text)
     result = None
     assert result, "Detection result is None"
 
 
 if __name__ == "__main__":
     main()
+
