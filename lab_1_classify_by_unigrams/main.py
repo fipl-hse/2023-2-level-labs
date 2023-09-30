12aa7299924fe57e6590fb19ae365402e3d5df0c
Arina Savina <arinasavina330@gmail.com>
2023-09-30 17:41:40 +0300

    Laboratory work #1, Savina Arina - 22FPL1

--- lab_1_classify_by_unigrams/main.py
+++ lab_1_classify_by_unigrams/main.py
@@ -1,4 +1,6 @@
 def tokenize(text: str) -> list[str] | None:
+    if not isinstance(text, str):
+        return None
     text = text.lower()
     tokens = list()
     for el in text:
@@ -8,14 +10,30 @@ def tokenize(text: str) -> list[str] | None:
 
 
 def calculate_frequencies(tokens: list[str] | None) -> dict[str, float] | None:
+    if not isinstance(tokens, list):
+        return None
+    for text in tokens:
+        if not isinstance(text, str):
+            return None
     frequency = {}
+    symbols = len(tokens)
     for el in tokens:
         if el not in frequency:
             frequency[el] = 1
         else:
             frequency[el] += 1
-
+    for k, v in frequency.items():
+        frequency[k] = v / symbols
     return frequency
 
 
-calculate_frequencies(tokenize('Hey!! How are you?'))
+def create_language_profile(language: str, text: str) -> dict[str, str | dict[str, float]] | None:
+    if not (isinstance(language, str) and isinstance(text, str)):
+        return None
+    freq = calculate_frequencies(tokenize(text))
+    profile = {'name': language,
+               'freq': freq}
+    return profile
+
+
+create_language_profile('en', 'jjhdf hkjddshu gdfd')
