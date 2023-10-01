40c27e937411c7f9e59f97b44c3077fe3e32d5fd
Dzianna7 <Dzianna7@mail.ru>
2023-10-01 14:38:18 +0300

    steps 3-4

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -2,7 +2,7 @@
 Language detection starter
 """
 
-
+from lab_1_classify_by_unigrams.main import tokenize
 def main() -> None:
     """
     Launches an implementation
@@ -13,8 +13,8 @@ def main() -> None:
         de_text = file_to_read_de.read()
     with open("assets/texts/unknown.txt", "r", encoding="utf-8") as file_to_read_unk:
         unknown_text = file_to_read_unk.read()
-    result = None
-    assert result, "Detection result is None"
+    result = tokenize(en_text)
+    print(result)
 
 
 if __name__ == "__main__":
