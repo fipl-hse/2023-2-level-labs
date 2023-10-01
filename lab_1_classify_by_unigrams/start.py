e55402b15f9a5506b0520cd3f8ed50228b6dab26
Dzianna7 <Dzianna7@mail.ru>
2023-10-01 14:39:45 +0300

    steps 3

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -13,8 +13,8 @@ def main() -> None:
         de_text = file_to_read_de.read()
     with open("assets/texts/unknown.txt", "r", encoding="utf-8") as file_to_read_unk:
         unknown_text = file_to_read_unk.read()
-    result = tokenize(en_text)
-    print(result)
+    tokenize_en_text = tokenize(en_text)
+    print(tokenize_en_text)
 
 
 if __name__ == "__main__":
