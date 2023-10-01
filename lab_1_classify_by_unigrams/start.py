860e5309ec8a2f2b746c068921ccdc17dbee2c48
kobyakova polina <polinagg.pk@gmail.com>
2023-10-01 21:33:13 +0300

    step 3

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -4,12 +4,17 @@ Language detection starter
 #from lab_1_classify_by_unigrams.main import create_language_profile
 import lab_1_classify_by_unigrams.main
 
+
 def main() -> None:
     """
     Launches an implementation
     """
     with open("assets/texts/en.txt", "r", encoding="utf-8") as file_to_read_en:
         en_text = file_to_read_en.read()
+        en_tokens = lab_1_classify_by_unigrams.main.tokenize(en_text)
+        print(en_tokens)
+        language_profile = lab_1_classify_by_unigrams.main.create_language_profile('en', en_text)
+        print(language_profile)
     with open("assets/texts/de.txt", "r", encoding="utf-8") as file_to_read_de:
         de_text = file_to_read_de.read()
     with open("assets/texts/unknown.txt", "r", encoding="utf-8") as file_to_read_unk:
@@ -17,9 +22,8 @@ def main() -> None:
     #print(tokenize(en_text))
     #print(calculate_frequencies(tokenize(en_text)))
     #print(create_language_profile('en', en_text))
-    en_tokens = lab_1_classify_by_unigrams.main.tokenize(en_text)
-    print(en_tokens)
-    result = None
+
+    #result = None
     #assert result, "Detection result is None"
 
 if __name__ == "__main__":
