cf2816cf38162cdf544c75784512aa55f21819d3
kobyakova polina <polinagg.pk@gmail.com>
2023-10-01 19:07:37 +0300

    step 3 changes

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -1,7 +1,7 @@
 """
 Language detection starter
 """
-from main import tokenize, calculate_frequencies, create_language_profile
+from main import create_language_profile
 
 def main() -> None:
     """
@@ -13,8 +13,8 @@ def main() -> None:
         de_text = file_to_read_de.read()
     with open("assets/texts/unknown.txt", "r", encoding="utf-8") as file_to_read_unk:
         unknown_text = file_to_read_unk.read()
-    print(tokenize(en_text))
-    print(calculate_frequencies(tokenize(en_text)))
+    #print(tokenize(en_text))
+    #print(calculate_frequencies(tokenize(en_text)))
     print(create_language_profile('en', en_text))
     result = None
     #assert result, "Detection result is None"
