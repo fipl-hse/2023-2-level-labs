b98689ccae73544c630cad036a379984e8503ee1
egorkostin24 <egorkostin24@gmail.com>
2023-10-02 20:14:11 +0300

    new updates

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 import string
 def tokenize(text):
-    if text == str:
+    if isinstance(text, str):
         text = text.lower()
         text1 = text.translate(str.maketrans('', '', string.punctuation))
         text2 = text1.replace(" ", "")
-        a = list(text2)
-        return a
+        tokens = list(text2)
+        return tokens
     else:
         return None
-tokenize("Hey! How are you?")
-def calculate_frequencies(a):
-   a = freq = dict((i, a.count(i)) for i in a)
-    print(a)
-calculate_frequencies()
+
+def calculate_frequencies(tokens: list[str] | None) -> dict[str, float] | None:
+    if isinstance(tokens, list) and all(isinstance(el,str) for el in tokens):
+        dictionary = {el: (tokens.count(el) / len(tokens)) for el in tokens}
+        return dictionary
+    else:
+        return None
+
+def create_language_profile(language: str, text: str) -> dict[str, str | dict[str, float]] | None:
+    if isinstance(language, str) and isinstance(text, str):
+        tokens = tokenize(text)
+        frequency_dict = calculate_frequencies(tokens)
+        language_profile = {'name': language, 'freq': frequency_dict}
+        return language_profile
+    else:
+        return None
 
 
 
