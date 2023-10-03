78e5f4a82b4c40b865584762f79d27dabceae5b5
mmarina <m.marina.oc14@mail.ru>
2023-10-03 09:24:29 +0300

    added fixes in start

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -1,23 +1,26 @@
 """
 Language detection starter
 """
-from lab_1_classify_by_unigrams.main import create_language_profile
-from lab_1_classify_by_unigrams.main import detect_language
+from lab_1_classify_by_unigrams.main import create_language_profile, detect_language
+
+
 def main() -> None:
     """
     Launches an implementation
     """
     with open("assets/texts/en.txt", "r", encoding="utf-8") as file_to_read_en:
         en_text = file_to_read_en.read()
-        en_profile = create_language_profile("en", en_text)
     with open("assets/texts/de.txt", "r", encoding="utf-8") as file_to_read_de:
         de_text = file_to_read_de.read()
-        de_profile = create_language_profile("de", de_text)
     with open("assets/texts/unknown.txt", "r", encoding="utf-8") as file_to_read_unk:
         unknown_text = file_to_read_unk.read()
-        unknown_profile = create_language_profile("unk", unknown_text)
-    result = detect_language(unknown_profile, en_profile, de_profile)
-    assert result, "Detection result is None"
+    en_profile = create_language_profile('en', en_text)
+    de_profile = create_language_profile('de', de_text)
+    unknown_profile = create_language_profile('unknown', unknown_text)
+    if (isinstance(en_profile, dict) and isinstance(de_profile, dict) and
+            isinstance(unknown_profile, dict)):
+        result = detect_language(unknown_profile, en_profile, de_profile)
+        assert result, "Detection result is None"
 
 
 if __name__ == "__main__":
