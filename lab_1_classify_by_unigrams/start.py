9db9f008010d19484fd2014181062413a2482e97
elena <eiermal@edu.hse.ru>
2023-10-03 23:06:53 +0300

    called functions

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -2,7 +2,7 @@
 Language detection starter
 """
 
-from lab_1_classify_by_unigrams.main import create_language_profile, detect_language
+from lab_1_classify_by_unigrams.main import create_language_profile, collect_profiles, detect_language_advanced, print_report
 
 
 def main() -> None:
@@ -16,19 +16,27 @@ def main() -> None:
     with open("assets/texts/unknown.txt", "r", encoding="utf-8") as file_to_read_unk:
         unknown_text = file_to_read_unk.read()
 
-    en_profile = create_language_profile('en', en_text)
-    de_profile = create_language_profile('de', de_text)
+    paths_to_profiles = [
+        "assets/profiles/de.json",
+        "assets/profiles/en.json",
+        "assets/profiles/es.json",
+        "assets/profiles/fr.json",
+        "assets/profiles/it.json",
+        "assets/profiles/ru.json",
+        "assets/profiles/tr.json"
+    ]
+
     unknown_profile = create_language_profile('unknown', unknown_text)
+    known_profiles = collect_profiles(paths_to_profiles)
 
-    if (isinstance(unknown_profile, dict)
-            and isinstance(en_profile, dict)
-            and isinstance(de_profile, dict)
+    if (
+            isinstance(unknown_profile, dict)
+            and isinstance(known_profiles, list)
     ):
-        result = detect_language(unknown_profile, en_profile, de_profile)
-        print(result)
-        assert result, "Detection result is None"
+        result = detect_language_advanced(unknown_profile, known_profiles)
+    print_report(result)
 
-
+    assert result, "Detection result is None"
 
 
 if __name__ == "__main__":
