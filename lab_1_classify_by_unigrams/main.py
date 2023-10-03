7fb18ec75dfd97f06d16791ae191a07f064ca8a8
Arina Savina <arinasavina330@gmail.com>
2023-10-03 23:33:44 +0300

    Laboratory work #1, Savina Arina - 22FPL1

--- lab_1_classify_by_unigrams/main.py
+++ lab_1_classify_by_unigrams/main.py
@@ -36,4 +36,36 @@ def create_language_profile(language: str, text: str) -> dict[str, str | dict[st
     return profile
 
 
-create_language_profile('en', 'jjhdf hkjddshu gdfd')
+def calculate_mse(predicted: list, actual: list) -> float | None:
+    if not len(predicted) == len(actual):
+        return None
+    if not (isinstance(predicted, list) and isinstance(actual, list)):
+        return None
+    difference = []
+    total = 0
+    for el1 in predicted:
+        for el2 in actual:
+            difference += (el2 - el1) ** 2
+    for dif in difference:
+        total += dif
+    mse = total / len(actual)
+    return mse
+
+
+def compare_profiles(
+        unknown_profile: dict[str, str | dict[str, float]],
+        profile_to_compare: dict[str, str | dict[str, float]]
+) -> float | None:
+    if not (isinstance(unknown_profile, dict) and isinstance(profile_to_compare, dict)):
+        return None
+    if ('name' and 'freq') not in unknown_profile and ('name' and 'freq') not in profile_to_compare:
+        return None
+    keys = sorted(set(unknown_profile.keys()) | set(profile_to_compare.keys()))
+    compare_un = []
+    compare_pr = []
+    for key in keys:
+        compare_un.append(unknown_profile.get(key, 0))
+        compare_pr.append(profile_to_compare.get(key, 0))
+    mse_calc = calculate_mse(compare_un, compare_pr)
+    return mse_calc
+
