692342fa07e58388d7c9f464b2613492a45a20be
Arina Savina <arinasavina330@gmail.com>
2023-10-04 16:20:30 +0300

    Laboratory work #1, Savina Arina - 22FPL1

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -2,9 +2,21 @@
 Language detection starter
 """
 
+from lab_1_classify_by_unigrams.main import create_language_profile
+
 
 def main() -> None:
-    pass
+    """
+    Launches an implementation
+    """
+    with open("assets/texts/en.txt", "r", encoding="utf-8") as file_to_read_en:
+        en_text = file_to_read_en.read()
+    with open("assets/texts/de.txt", "r", encoding="utf-8") as file_to_read_de:
+        de_text = file_to_read_de.read()
+    with open("assets/texts/unknown.txt", "r", encoding="utf-8") as file_to_read_unk:
+        unknown_text = file_to_read_unk.read()
+    result = create_language_profile('en', en_text)
+    assert result, "Detection result is None"
 
 
 if __name__ == "__main__":
