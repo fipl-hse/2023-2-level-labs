6e700f4bd4eaba0535dd6e6ea0a0eb9e655da721
logunova-anastasia <lgnvssst@gmail.com>
2023-10-04 21:40:35 +0300

    edited style

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -1,8 +1,8 @@
 """
 Language detection starter
 """
-from lab_1_classify_by_unigrams.main import create_language_profile, collect_profiles, detect_language_advanced, \
-    print_report
+from lab_1_classify_by_unigrams.main import (create_language_profile, collect_profiles,
+                                             detect_language_advanced, print_report)
 
 
 def main() -> None:
@@ -16,8 +16,9 @@ def main() -> None:
     with open("assets/texts/unknown.txt", "r", encoding="utf-8") as file_to_read_unk:
         unknown_text = file_to_read_unk.read()
     unknown_profile = create_language_profile('unknown', unknown_text)
-    language_profiles = ['assets/profiles/es.json', 'assets/profiles/de.json', 'assets/profiles/en.json',
-                         'assets/profiles/fr.json', 'assets/profiles/it.json', 'assets/profiles/ru.json',
+    language_profiles = ['assets/profiles/es.json', 'assets/profiles/de.json',
+                         'assets/profiles/en.json', 'assets/profiles/fr.json',
+                         'assets/profiles/it.json', 'assets/profiles/ru.json',
                          'assets/profiles/tr.json']
     profiles = collect_profiles(language_profiles)
     if isinstance(unknown_profile, dict) and isinstance(profiles, list):
