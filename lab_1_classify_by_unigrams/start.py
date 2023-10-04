374815280fa3922bacc5697c960256ec5b91565f
kobyakova polina <polinagg.pk@gmail.com>
2023-10-04 09:54:21 +0300

    step 3

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -2,7 +2,7 @@
 Language detection starter
 """
 
-import lab_1_classify_by_unigrams.main
+from lab_1_classify_by_unigrams.main import tokenize, calculate_frequencies, create_language_profile
 
 
 def main() -> None:
@@ -11,20 +11,22 @@ def main() -> None:
     """
     with open("assets/texts/en.txt", "r", encoding="utf-8") as file_to_read_en:
         en_text = file_to_read_en.read()
-        en_tokens = lab_1_classify_by_unigrams.main.tokenize(en_text)
-        print(en_tokens)
-        language_profile = lab_1_classify_by_unigrams.main.create_language_profile('en', en_text)
-        print(language_profile)
     with open("assets/texts/de.txt", "r", encoding="utf-8") as file_to_read_de:
         de_text = file_to_read_de.read()
     with open("assets/texts/unknown.txt", "r", encoding="utf-8") as file_to_read_unk:
         unknown_text = file_to_read_unk.read()
-    #print(tokenize(en_text))
-    #print(calculate_frequencies(tokenize(en_text)))
-    #print(create_language_profile('en', en_text))
+
+    tokens = tokenize(en_text)
+    print(tokens)
+    print(calculate_frequencies(tokens))
+    print(create_language_profile('en', en_text))
 
     #result = None
     #assert result, "Detection result is None"
+    #en_tokens = lab_1_classify_by_unigrams.main.tokenize(en_text)
+    #print(en_tokens)
+    #language_profile = lab_1_classify_by_unigrams.main.create_language_profile('en', en_text)
+    #print(language_profile)
 
 if __name__ == "__main__":
     main()
