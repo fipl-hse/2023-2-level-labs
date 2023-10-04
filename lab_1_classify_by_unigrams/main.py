1b73eada956a8b1404f130582c51b6e7c6324074
sofianurtdinova <asnurtdinova2@gmail.com>
2023-10-05 02:44:40 +0300

    Laboratory work #1, Sofia Nurtdinova - 22FPL2

--- lab_1_classify_by_unigrams/main.py
+++ lab_1_classify_by_unigrams/main.py
@@ -119,12 +119,15 @@ def detect_language(
     :return: a language
     """
 
-    if not isinstance(unknown_profile, dict) or not isinstance(profile_1, dict) or not isinstance(profile_2, dict):
+    if (not isinstance(unknown_profile, dict)
+            or not isinstance(profile_1, dict)
+            or not isinstance(profile_2, dict)):
         return None
     mse_1 = compare_profiles(unknown_profile, profile_1)
     mse_2 = compare_profiles(unknown_profile, profile_2)
     if not isinstance(mse_1, float) or not isinstance(mse_2, float):
         return None
+
     if mse_1 < mse_2:
         return str(profile_1['name'])
     if mse_2 < mse_1:
