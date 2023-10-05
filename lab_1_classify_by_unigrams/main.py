80c7fc12c00c4c207c3c051fb9edb03033fcf33b
Arina Savina <arinasavina330@gmail.com>
2023-10-05 17:58:21 +0300

    Laboratory work #1, Savina Arina - 22FPL1

--- lab_1_classify_by_unigrams/main.py
+++ lab_1_classify_by_unigrams/main.py
@@ -106,20 +106,23 @@ def detect_language(
     :param profile_2: a dictionary of a known profile
     :return: a language
     """
-    if not (isinstance(unknown_profile, dict) and
+    if not (
+            isinstance(unknown_profile, dict) and
             isinstance(profile_1, dict) and
-            isinstance(profile_2, dict)):
+            isinstance(profile_2, dict)
+    ):
         return None
     mse_1 = compare_profiles(unknown_profile, profile_1)
     mse_2 = compare_profiles(unknown_profile, profile_2)
-    if mse_1 == mse_2:
-        names = [profile_1.get('name'), profile_2.get('name')]
-        names = str(sorted(names))
-        return names
-    if mse_2 > mse_1:
-        return profile_1.get('name')
-    if mse_1 > mse_2:
-        return profile_2.get('name')
+    if isinstance(mse_1, float) and isinstance(mse_2, float):
+        if mse_1 == mse_2:
+            names = [profile_1.get('name'), profile_2.get('name')]
+            names = str(sorted(names))
+            return names
+        if mse_2 > mse_1:
+            return str(profile_1.get('name'))
+        if mse_1 > mse_2:
+            return str(profile_2.get('name'))
 
 
 def load_profile(path_to_file: str) -> dict | None:
