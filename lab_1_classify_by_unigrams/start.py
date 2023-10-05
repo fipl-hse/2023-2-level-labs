a566adcd4a10286bb935a35e7582c82d38860c1e
alesiamashkovtseva <alesiamashkovtseva@gmail.com>
2023-10-05 11:19:39 +0300

    fixing import style checks

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -1,8 +1,7 @@
 """
 Language detection starter
 """
-from lab_1_classify_by_unigrams.main import create_language_profile
-from lab_1_classify_by_unigrams.main import detect_language
+import lab_1_classify_by_unigrams.main
 
 
 def main() -> None:
@@ -11,16 +10,16 @@ def main() -> None:
     """
     with open("assets/texts/en.txt", "r", encoding="utf-8") as file_to_read_en:
         en_text = file_to_read_en.read()
-        en_profile = create_language_profile('en', en_text)
+        en_profile = lab_1_classify_by_unigrams.main.create_language_profile('en', en_text)
     with open("assets/texts/de.txt", "r", encoding="utf-8") as file_to_read_de:
         de_text = file_to_read_de.read()
-        de_profile = create_language_profile('de', de_text)
+        de_profile = lab_1_classify_by_unigrams.main.create_language_profile('de', de_text)
     with open("assets/texts/unknown.txt", "r", encoding="utf-8") as file_to_read_unk:
         unknown_text = file_to_read_unk.read()
-        unknown_profile = create_language_profile('unknown', unknown_text)
+        unknown_profile = lab_1_classify_by_unigrams.main.create_language_profile('unknown', unknown_text)
     result = None
     assert result, "Detection result is None"
-    result = detect_language(unknown_profile, en_profile, de_profile)
+    result = lab_1_classify_by_unigrams.main.detect_language(unknown_profile, en_profile, de_profile)
     return result
 
 if __name__ == "__main__":
