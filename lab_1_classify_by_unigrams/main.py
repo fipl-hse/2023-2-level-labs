a203271e6e8ac04de34e7ce74bafcd1078c1539e
Arina Savina <arinasavina330@gmail.com>
2023-10-05 18:46:06 +0300

    Laboratory work #1, Savina Arina - 22FPL1

--- lab_1_classify_by_unigrams/main.py
+++ lab_1_classify_by_unigrams/main.py
@@ -114,14 +114,12 @@ def detect_language(
         return None
     mse_1 = compare_profiles(unknown_profile, profile_1)
     mse_2 = compare_profiles(unknown_profile, profile_2)
-    names = [profile_1.get('name'), profile_2.get('name')]
-    names = str(sorted(names))
     if isinstance(mse_1, float) and isinstance(mse_2, float):
         if mse_2 > mse_1:
             return str(profile_1.get('name'))
         if mse_1 > mse_2:
             return str(profile_2.get('name'))
-    return names
+    return str(sorted([profile_1['name'], profile_2['name']]))
 
 
 def load_profile(path_to_file: str) -> dict | None:
