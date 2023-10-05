f303e040862305713ecc2cff27079348ea635e16
слава угольников <viacheslav.ugolnickoff@gmail.com>
2023-10-05 16:56:28 +0300

    start.py is changed again in an attempt to solve problems with import
    Signed-off-by: слава угольников <viacheslav.ugolnickoff@gmail.com>

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -1,7 +1,7 @@
 """
 Language detection starter
 """
-from lab_1_classify_by_unigrams.main import tokenize, create_language_profile
+from lab_1_classify_by_unigrams.main import tokenize
 
 
 def main() -> None:
