c8dbab650ed941dc7196c59d6b777e7445a809b5
Sofya0borodina <134336286+Sofya0borodina@users.noreply.github.com>
2023-10-05 14:02:57 +0300

    Update start.py

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -1,7 +1,7 @@
 """
 Language detection starter
 """
-from lab_1_classify_by_unigrams/main.py import create_language_profile, detect_language
+from lab_1_classify_by_unigrams.main import create_language_profile, detect_language
 
 
 def main() -> None:
