6b2e0f4e22b74c37d9ee654ef3da66acd657c5aa
egor-solo <dotabezgranic@mail.ru>
2023-10-05 13:05:17 +0300

    steps 1-3

--- lab_1_classify_by_unigrams/main.py
+++ lab_1_classify_by_unigrams/main.py
@@ -12,14 +12,33 @@ def tokenize(text: str) -> list[str] | None:
     :param text: a text
     :return: a list of lower-cased tokens without punctuation
     """
-
+    if not isinstance(text, str):
+        return None
+    else:
+        tokens = (text.lower())
+        punctuation = " !#$%&'\'()*+,-./:;<=>?@[\\]^_`{|}~1234567890"
+        for token in tokens:
+            if token in punctuation:
+                tokens = tokens.replace(token, '')
+        tokens = list(tokens)
+    return tokens
 def calculate_frequencies(tokens: list[str] | None) -> dict[str, float] | None:
     """
     Calculates frequencies of given tokens
     :param tokens: a list of tokens
     :return: a dictionary with frequencies
     """
-
+    freq = {}
+    if not isinstance(tokens, list):
+        return None
+    for token in tokens:
+        if not isinstance(token, str):
+            return None
+    for token in tokens:
+        freq[token] = (1 if token not in freq else freq[token] + 1)
+    for token in freq:
+        freq[token] /= len(tokens)
+    return freq
 
 def create_language_profile(language: str, text: str) -> dict[str, str | dict[str, float]] | None:
     """
@@ -28,7 +47,12 @@ def create_language_profile(language: str, text: str) -> dict[str, str | dict[st
     :param text: a text
     :return: a dictionary with two keys – name, freq
     """
-
+    if not isinstance(tokens, list):
+        return None
+    frequencies = calculate_frequencies(tokenize(text))
+    if isinstance(frequencies, dict):
+        return {'name': language, 'freq': frequencies}
+    return None
 
 def calculate_mse(predicted: list, actual: list) -> float | None:
     """
