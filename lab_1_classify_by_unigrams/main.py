c37d91e87db5ffb6686e34b259469738e798d177
Arina Savina <arinasavina330@gmail.com>
2023-10-05 21:37:22 +0300

    Laboratory work #1, Savina Arina - 22FPL1

--- lab_1_classify_by_unigrams/main.py
+++ lab_1_classify_by_unigrams/main.py
@@ -81,7 +81,7 @@ def compare_profiles(
     """
     if not (isinstance(unknown_profile, dict) and isinstance(profile_to_compare, dict)):
         return None
-    if not ('name' and 'freq') in unknown_profile or not ('name' and 'freq') in profile_to_compare:
+    if not ('name' or 'freq') in unknown_profile or not ('name' or 'freq')in profile_to_compare:
         return None
     keys = sorted(set(unknown_profile['freq'].keys()) | set(profile_to_compare['freq'].keys()))
     comparison_un = []
