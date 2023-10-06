cff0371fb8689f4040f9ad52c5b763b5b1dbe672
Ratmir Akhmatzyanov <ratmirahmatzanov11102003@gmail.com>
2023-10-06 04:03:23 +0300

    работа на 4

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -1,7 +1,7 @@
 """
 Language detection starter
 """
-
+from lab_1_classify_by_unigrams.main import tokenize
 
 def main() -> None:
     """
@@ -13,7 +13,7 @@ def main() -> None:
         de_text = file_to_read_de.read()
     with open("assets/texts/unknown.txt", "r", encoding="utf-8") as file_to_read_unk:
         unknown_text = file_to_read_unk.read()
-    result = None
+    result = tokenize(en_text)
     assert result, "Detection result is None"
 
 
