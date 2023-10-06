820cc6364c2361cb1bd8f187b0acdc278cbb283f
tima-baranov <timobaranov@gmail.com>
2023-10-06 08:02:05 +0300

    changed realization for tokenize function

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -14,7 +14,6 @@ def main() -> None:
         de_text = file_to_read_de.read()
     with open("assets/texts/unknown.txt", "r", encoding="utf-8") as file_to_read_unk:
         unknown_text = file_to_read_unk.read()
-    result = None
     result = tokenize('en', en_text)
     assert result, "Detection result is None"
 
