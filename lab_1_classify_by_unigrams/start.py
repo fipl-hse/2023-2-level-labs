b23683737c1a4be6b8d3bc2222e5987931406bdb
Ulyana <ulyana.nagornaya@gmail.com>
2023-10-06 11:56:19 +0300

    revert start.py

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -1,8 +1,10 @@
 """
 Language detection starter
 """
-from lab_1_classify_by_unigrams.main import (collect_profiles, create_language_profile,
-                                             detect_language_advanced, print_report)
+from lab_1_classify_by_unigrams.main import (collect_profiles,
+                                             create_language_profile,
+                                            detect_language_advanced,
+                                            print_report)
 
 
 def main() -> None:
@@ -13,22 +15,22 @@ def main() -> None:
                          'assets/profiles/fr.json','assets/profiles/it.json',
                          'assets/profiles/ru.json','assets/profiles/tr.json']
 
-    # with open("assets/texts/en.txt", "r", encoding="utf-8") as file_to_read_en:
-    #     en_text = file_to_read_en.read()
-    # with open("assets/texts/de.txt", "r", encoding="utf-8") as file_to_read_de:
-    #     de_text = file_to_read_de.read()
+    with open("assets/texts/en.txt", "r", encoding="utf-8") as file_to_read_en:
+        en_text = file_to_read_en.read()
+    with open("assets/texts/de.txt", "r", encoding="utf-8") as file_to_read_de:
+        de_text = file_to_read_de.read()
     with open("assets/texts/unknown.txt", "r", encoding="utf-8") as file_to_read_unk:
         unknown_text = file_to_read_unk.read()
 
-    # en_profile = create_language_profile('en', en_text)
-    # de_profile = create_language_profile('de', de_text)
-    # unknown_profile = create_language_profile('unknown', unknown_text)
+    en_profile = create_language_profile('en', en_text)
+    de_profile = create_language_profile('de', de_text)
+    unknown_profile = create_language_profile('unknown', unknown_text)
 
-    # if (isinstance(en_profile, dict) and isinstance(de_profile, dict) \
-    # and isinstance(unknown_profile, dict)):
-    #     result = detect_language(unknown_profile, en_profile, de_profile)
-    # else:
-    #     result = None
+    if (isinstance(en_profile, dict) and isinstance(de_profile, dict) \
+    and isinstance(unknown_profile, dict)):
+        result = detect_language(unknown_profile, en_profile, de_profile)
+    else:
+        result = None
 
     unknown_profile = create_language_profile('unknown language', unknown_text)
     known_profiles = collect_profiles(paths_to_profiles)
