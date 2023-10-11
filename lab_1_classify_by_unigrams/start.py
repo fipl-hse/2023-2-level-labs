9de3541d65dcc654e0d5305f6f8f6aa888be2eb6
artyomtugaryov <artyomtugaryov@users.noreply.github.com>
2023-10-11 11:02:56 +0300

    checkout labs from the origin repository

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -1,8 +1,6 @@
 """
 Language detection starter
 """
-from lab_1_classify_by_unigrams.main import (collect_profiles, create_language_profile,
-                                             detect_language_advanced, print_report)
 
 from lab_1_classify_by_unigrams.main import (collect_profiles, create_language_profile,
                                              detect_language_advanced, print_report)
@@ -12,6 +10,10 @@ def main() -> None:
     """
     Launches an implementation
     """
+    with open("assets/texts/en.txt", "r", encoding="utf-8") as file_to_read_en:
+        en_text = file_to_read_en.read()
+    with open("assets/texts/de.txt", "r", encoding="utf-8") as file_to_read_de:
+        de_text = file_to_read_de.read()
     with open("assets/texts/unknown.txt", "r", encoding="utf-8") as file_to_read_unk:
         unknown_text = file_to_read_unk.read()
 
