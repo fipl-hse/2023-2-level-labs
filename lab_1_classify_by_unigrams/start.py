3a421e027fd5af99c63eb2b3f2c98965547a0092
artyomtugaryov <artyomtugaryov@users.noreply.github.com>
2023-10-11 09:50:42 +0300

    checkout labs from the origin repository

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -3,9 +3,6 @@ Language detection starter
 """
 from lab_1_classify_by_unigrams.main import create_language_profile
 
-from lab_1_classify_by_unigrams.main import (collect_profiles, create_language_profile,
-                                             detect_language_advanced, print_report)
-
 
 def main() -> None:
     """
@@ -20,22 +17,6 @@ def main() -> None:
     result = create_language_profile('en', en_text)
     assert result, "Detection result is None"
 
-    json_paths = ['assets/profiles/de.json',
-                  'assets/profiles/en.json',
-                  'assets/profiles/es.json',
-                  'assets/profiles/fr.json',
-                  'assets/profiles/it.json',
-                  'assets/profiles/ru.json',
-                  'assets/profiles/tr.json']
-
-    unknown = create_language_profile('unknown', unknown_text)
-    known = collect_profiles(json_paths)
-    if isinstance(unknown, dict) and isinstance(known, list):
-        result = detect_language_advanced(unknown, known)
-    if result:
-        print_report(result)
-
-    assert result, "Detection result is None"
 
 if __name__ == "__main__":
     main()
