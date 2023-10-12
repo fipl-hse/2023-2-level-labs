a60331137d76f04cc2e47b8c65ee2ec01d571f5c
Vladays <tsvetk1@yandex.ru>
2023-10-12 22:01:25 +0300

    Merge remote-tracking branch 'origin/main'

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -14,20 +14,16 @@ def main() -> None:
     with open("assets/texts/unknown.txt", "r", encoding="utf-8") as file_to_read_unk:
         unknown_text = file_to_read_unk.read()
     result = None
+    unknownt = create_language_profile("unknown", unknown_text)
+    givente = create_language_profile("en", en_text)
+    giventd = create_language_profile("de", de_text)
+    answer = detect_language(unknownt, givente, giventd)
+    print(answer)
+
     assert result, "Detection result is None"
 
     import main
-    main.tokenize(text: str)
-    main.calculate_frequencies(tokens: list[str] | None)
-    main.create_language_profile(language: str, text: str)
-    main.calculate_mse(predicted: list, actual: list)
-    main.compare_profiles(
-        unknown_profile: dict[str, str | dict[str, float]],
-    profile_to_compare: dict[str, str | dict[str, float]])
-    main.detect_language(
-        unknown_profile: dict[str, str | dict[str, float]],
-        profile_1: dict[str, str | dict[str, float]],
-        profile_2: dict[str, str | dict[str, float]],)
+
 
 
 if __name__ == "__main__":
