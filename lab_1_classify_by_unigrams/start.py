f07551c8b6ab8246435d937d92b85de31f61b3e6
Dzianna7 <Dzianna7@mail.ru>
2023-10-13 11:09:52 +0300

    fixes to improve the mark

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -1,9 +1,10 @@
 """
 Language detection starter
 """
+from lab_1_classify_by_unigrams.main import detect_language
 
 from lab_1_classify_by_unigrams.main import create_language_profile
-from lab_1_classify_by_unigrams.main import detect_language
+
 
 def main() -> None:
     """
