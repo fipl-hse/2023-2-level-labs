1b1253b1a218964487df99bcf6f775996fde55ad
Соня <saonofrienko@edu.hse.ru>
2023-10-13 04:37:19 +0300

    final changes, I have changed the start module

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -4,6 +4,8 @@ Language detection starter
 
 
 from lab_1_classify_by_unigrams.main import (create_language_profile, detect_language)
+
+
 def main() -> None:
     """
     Launches an implementation
