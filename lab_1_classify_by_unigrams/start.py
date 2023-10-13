e312e9b81f7d57318e32845ecfe4bb1e8fd72ba6
ogabekmatyoqubov <ogabekmatyoqubov577@gmail.com>
2023-10-13 09:40:50 +0300

    Laboratory work #1, Matyakubov Ogabek - 22FPL2

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -2,7 +2,7 @@
 Language detection starter
 """
 
-
+from lab_1_classify_by_unigrams.main import create_language_profile
 def main() -> None:
     """
     Launches an implementation
@@ -13,7 +13,7 @@ def main() -> None:
         de_text = file_to_read_de.read()
     with open("assets/texts/unknown.txt", "r", encoding="utf-8") as file_to_read_unk:
         unknown_text = file_to_read_unk.read()
-    result = None
+    result = create_language_profile("en", en_text)
     assert result, "Detection result is None"
 
 
