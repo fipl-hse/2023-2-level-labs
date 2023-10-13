092155c7c6fad1a58b150caeb1f2f05ad5e44522
Chuvachin <dakorneva@gmail.com>
2023-10-13 09:27:03 +0300

    Merge remote-tracking branch 'origin/main'

--- lab_1_classify_by_unigrams/main.py
+++ lab_1_classify_by_unigrams/main.py
@@ -45,8 +45,9 @@ def create_language_profile(language: str, text: str) -> dict[str, str | dict[st
     """
     if not isinstance(language, str) or not isinstance(text, str):
         return None
-
-    return {(("name", language), ("freq", calculate_frequencies(tokenize(text))))}
+    tokens = (tokenize(text))
+    frequency_dict = calculate_frequencies(tokens)
+    return dict(("name", language), ("freq", frequency_dict))
 
 
 def calculate_mse(predicted: list, actual: list) -> float | None:
