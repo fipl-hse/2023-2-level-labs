1a4b880ff3d0910e1efe671c8312b4fc01959943
Vladays <tsvetk1@yandex.ru>
2023-10-13 12:03:32 +0300

    Merge remote-tracking branch 'origin/main'

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -7,6 +7,12 @@ def main() -> None:
     """
     Launches an implementation
     """
+    with open("assets/texts/en.txt", "r", encoding="utf-8") as file_to_read_en:
+        en_text = file_to_read_en.read()
+    with open("assets/texts/de.txt", "r", encoding="utf-8") as file_to_read_de:
+        de_text = file_to_read_de.read()
+    with open("assets/texts/unknown.txt", "r", encoding="utf-8") as file_to_read_unk:
+        unknown_text = file_to_read_unk.read()
     json_paths = ['assets/profiles/de.json',
                   'assets/profiles/en.json',
                   'assets/profiles/es.json',
@@ -20,11 +26,10 @@ def main() -> None:
     if isinstance(unknown, dict) and isinstance(known, list):
         result = detect_language(unknown, known)
     if result:
-        print_report(result)
+        print(result)
 
     assert result, "Detection result is None"
 
-    import main
 
 
 
