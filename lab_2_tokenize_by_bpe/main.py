e71d2400304a93f9e8f55ec1b102db98ff73a79b
ogabekmatyoqubov <ogabekmatyoqubov577@gmail.com>
2023-10-13 21:57:30 +0300

    Laboratory work #2, Matyakubov Ogabek - 22FPL2

--- lab_2_tokenize_by_bpe/main.py
+++ lab_2_tokenize_by_bpe/main.py
@@ -14,7 +14,11 @@ def prepare_word(
     :param end_of_word: a token that signifies the end of word
     :return: preprocessed word
     """
-    print('hello worl')
+    if not isinstance(raw_word, str) or not (isinstance(
+            start_of_word, str) or start_of_word is None) or not (
+            isinstance(end_of_word, str) or end_of_word is None):
+        return None
+
 
 def collect_frequencies(
     text: str, start_of_word: str | None, end_of_word: str
