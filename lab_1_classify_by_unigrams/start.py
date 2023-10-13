c530fae1b45dc53baaf7bcc955a4e8be99326cf8
tima-baranov <timobaranov@gmail.com>
2023-10-13 08:38:29 +0300

    i hope it will work

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -15,6 +15,7 @@ def main() -> None:
     with open("assets/texts/unknown.txt", "r", encoding="utf-8") as file_to_read_unk:
         unknown_text = file_to_read_unk.read()
     result = print(tokenize(en_text))
+    assert result, "Detection result is None"
 
 if __name__ == "__main__":
     main()
