04b886aae777d00cdf7c52df1487f524e166cda1
tima-baranov <timobaranov@gmail.com>
2023-10-13 09:15:20 +0300

    /

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -14,7 +14,9 @@ def main() -> None:
         de_text = file_to_read_de.read()
     with open("assets/texts/unknown.txt", "r", encoding="utf-8") as file_to_read_unk:
         unknown_text = file_to_read_unk.read()
-    result = print(tokenize(en_text))
+    result = tokenize(en_text)
+    assert result, "Detection result is None"
+
 
 if __name__ == "__main__":
     main()
