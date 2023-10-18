33445ffeaedd1623a06d5031a7b5cd37c2bd72b1
Vladays <tsvetk1@yandex.ru>
2023-10-18 22:45:46 +0300

    Merge remote-tracking branch 'origin/main'

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -2,7 +2,7 @@
 Language detection starter
 """
 
-from lab_1_classify_by_unigrams.main import (create_language_profile, detect_language)
+from lab_1_classify_by_unigrams.main import create_language_profile, detect_language
 
 
 def main() -> None:
@@ -12,7 +12,8 @@ def main() -> None:
 
     en_text = file_to_read_en.read()
     de_text = file_to_read_de.read()
-    unknown_text = file_to_read_unk.read()
+    with open("assets/texts/unknown.txt", "r", encoding="utf-8") as file_to_read_unk:
+        unknown_text = file_to_read_unk.read()
     unkmown_p = create_language_profile("un", unknown_text)
     eng_p = create_language_profile("en", en_text)
     deu_p = create_language_profile("de", de_text)
