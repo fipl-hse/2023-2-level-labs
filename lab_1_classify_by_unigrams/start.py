7796c48b38048c06621f1504f733027bbdcd265d
Vladays <tsvetk1@yandex.ru>
2023-10-18 22:52:38 +0300

    Merge remote-tracking branch 'origin/main'

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -9,17 +9,20 @@ def main() -> None:
     """
     Launches an implementation
     """
-
-    en_text = file_to_read_en.read()
-    de_text = file_to_read_de.read()
+    with open("assets/texts/en.txt", "r", encoding="utf-8") as file_to_read_en:
+        en_text = file_to_read_en.read()
+    with open("assets/texts/de.txt", "r", encoding="utf-8") as file_to_read_de:
+        de_text = file_to_read_de.read()
     with open("assets/texts/unknown.txt", "r", encoding="utf-8") as file_to_read_unk:
         unknown_text = file_to_read_unk.read()
-    unkmown_p = create_language_profile("un", unknown_text)
-    eng_p = create_language_profile("en", en_text)
-    deu_p = create_language_profile("de", de_text)
-    if (isinstance(unkmown_p, dict) and
-            isinstance(eng_p, dict) and
-            isinstance(deu_p, dict)):
+    with open("assets/texts/unknown.txt", "r", encoding="utf-8") as file_to_read_unk:
+        unknown_text = file_to_read_unk.read()
+    unk = create_language_profile("un", unknown_text)
+    eng = create_language_profile("en", en_text)
+    deu = create_language_profile("de", de_text)
+    if (isinstance(unk, dict) and
+            isinstance(eng, dict) and
+            isinstance(deu, dict)):
         result = detect_language(unk, eng, deu)
         assert result, "Detection result is None"
 
