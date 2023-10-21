10877ab863d51755faf23eb6e4d328696e6f8613
Margarita Velmisova <margaritavelmisova@gmail.com>
2023-10-22 01:58:11 +0300

    fixing mentor's comments

--- lab_2_tokenize_by_bpe/main.py
+++ lab_2_tokenize_by_bpe/main.py
@@ -82,6 +82,9 @@ def count_tokens_pairs(
 
             pair_frequency_dict[pair_token] += word_frequencies[word_tokens]
 
+    if not pair_frequency_dict:
+        return None
+
     return pair_frequency_dict
 
 
