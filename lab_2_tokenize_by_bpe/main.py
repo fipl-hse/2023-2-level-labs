b75cb7cc7dd6dcf48d9b1c6b7908ef2b9c485a7e
tima-baranov <timobaranov@gmail.com>
2023-10-25 20:31:05 +0300

    add realization for prepare_word and collect_frequencies functions

--- lab_2_tokenize_by_bpe/main.py
+++ lab_2_tokenize_by_bpe/main.py
@@ -14,8 +14,15 @@ def prepare_word(
     :param end_of_word: a token that signifies the end of word
     :return: preprocessed word
     """
-
-
+    if not isinstance(raw_word, str) or not isinstance(start_of_word, str | None) \
+            or not isinstance(end_of_word, str | None):
+        return None
+    tokens = list(raw_word)
+    if start_of_word is not None:
+        tokens.insert(0, start_of_word)
+    if end_of_word is not None:
+        tokens.append(end_of_word)
+    return tuple(tokens)
 def collect_frequencies(
     text: str, start_of_word: str | None, end_of_word: str
 ) -> dict[tuple[str, ...], int] | None:
@@ -26,8 +33,15 @@ def collect_frequencies(
     :param end_of_word: a token that signifies the end of word
     :return: dictionary in the form of <preprocessed word: number of occurrences>
     """
-
-
+    if not isinstance(text, str) or not isinstance(start_of_word, str | None) or not isinstance(end_of_word, str):
+        return None
+    freq_dictionary = {}
+    for i in text.split():
+        preprocessed_word = prepare_word(i, start_of_word, end_of_word)
+        if not preprocessed_word:
+            return None
+        freq_dictionary.update({preprocessed_word: text.split().count(i)})
+    return freq_dictionary
 def count_tokens_pairs(
     word_frequencies: dict[tuple[str, ...], int]
 ) -> dict[tuple[str, str], int] | None:
