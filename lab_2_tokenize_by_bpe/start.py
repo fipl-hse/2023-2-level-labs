66374571fd34b6519598f6f9c71d23c172b5fe15
Anna Azimkova <annaazimkova@gmail.com>
2023-10-25 17:25:14 +0300

    start

--- lab_2_tokenize_by_bpe/start.py
+++ lab_2_tokenize_by_bpe/start.py
@@ -2,7 +2,7 @@
 BPE Tokenizer starter
 """
 from pathlib import Path
-
+from lab_2_tokenize_by_bpe.main import collect_frequencies, train
 
 def main() -> None:
     """
@@ -12,7 +12,8 @@ def main() -> None:
     with open(assets_path / 'text.txt', 'r', encoding='utf-8') as text_file:
         text = text_file.read()
 
-    result = None
+    result = train(collect_frequencies(text, None, '</s>'), 100)
+    print(result)
     assert result, "Encoding is not working"
 
 
