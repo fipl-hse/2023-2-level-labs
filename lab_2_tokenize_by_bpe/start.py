cedeb7cbce536a25b4fc08ff5e7dc152a2665031
Sofya0borodina <134336286+Sofya0borodina@users.noreply.github.com>
2023-10-25 18:20:33 +0300

    added fix

--- lab_2_tokenize_by_bpe/start.py
+++ lab_2_tokenize_by_bpe/start.py
@@ -2,7 +2,9 @@
 BPE Tokenizer starter
 """
 from pathlib import Path
-from lab_2_tokenize_by_bpe.main import (prepare_word, collect_frequencies, count_tokens_pairs, merge_tokens, train)
+from lab_2_tokenize_by_bpe.main import (prepare_word, collect_frequencies,
+                                        count_tokens_pairs, merge_tokens, train)
+
 
 
 def main() -> None:
