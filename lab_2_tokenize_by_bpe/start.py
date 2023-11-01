554696c8ac2a5ac6ba056bd751b277d78db6c5c4
alesiamashkovtseva <alesiamashkovtseva@gmail.com>
2023-11-01 23:53:57 +0300

    start.py fixed

--- lab_2_tokenize_by_bpe/start.py
+++ lab_2_tokenize_by_bpe/start.py
@@ -17,7 +17,7 @@ def main() -> None:
     word_frequencies = main_py.collect_frequencies(text, None, '</s>')
     vocabulary = main_py.get_vocabulary(word_frequencies, '<unk>')
 
-    with open(assets_path / 'secrets' / 'secret.txt', 'r', encoding='utf-8') as secret_file:
+    with open(assets_path / 'secrets' / 'secret_1.txt', 'r', encoding='utf-8') as secret_file:
         secret = secret_file.read()
 
     result = main_py.decode(secret.split(' '), vocabulary, '</s>')
