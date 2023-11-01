2cb18fad5dd2de11bf993831c892b9c4bd1102b9
kramkova <m.kramkova@gmail.com>
2023-11-02 01:47:57 +0300

    start.py attempt

--- lab_2_tokenize_by_bpe/start.py
+++ lab_2_tokenize_by_bpe/start.py
@@ -1,18 +1,24 @@
 """
 BPE Tokenizer starter
 """
-from pathlib import Path
+from main import load_vocabulary, encode, decode, calculate_bleu
 
 
 def main() -> None:
     """
     Launches an implementation
     """
-    assets_path = Path(__file__).parent / 'assets'
-    with open(assets_path / 'text.txt', 'r', encoding='utf-8') as text_file:
-        text = text_file.read()
-
-    result = None
+    with open('./assets/for_translation_ru_raw.txt', encoding='utf-8') as prediction_file:
+        text = prediction_file.read()
+    vocabulary = load_vocabulary('./assets/vocab.json')
+    encoded_text = encode(text, vocabulary, '\u2581', None, '<unk>')
+    with open('./assets/for_translation_en_encoded.txt', encoding='utf-8') as response_file:
+        model_response_str = response_file.read().split()
+    model_response = [int(token) for token in model_response_str]
+    decoded_response = decode(model_response, vocabulary, None)
+    with open('./assets/for_translation_en_raw.txt', encoding='utf-8') as ideal_file:
+        ideal_translation = ideal_file.read()
+    result = calculate_bleu(decoded_response.replace('\u2581', ' '), ideal_translation)
     assert result, "Encoding is not working"
 
 
