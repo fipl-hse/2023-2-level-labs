1838733b7ade1c22ef2dfe03cec15d7772c573a3
Ulyana-Nagornaya <ulyana.nagornaya@gmail.com>
2023-11-02 10:14:29 +0300

    fixing issues

--- lab_2_tokenize_by_bpe/start.py
+++ lab_2_tokenize_by_bpe/start.py
@@ -4,7 +4,7 @@ BPE Tokenizer starter
 from pathlib import Path
 
 from lab_2_tokenize_by_bpe.main import \
- (collect_frequencies, decode, get_vocabulary, train)
+ collect_frequencies, decode, get_vocabulary, train
 
 
 def main() -> None:
