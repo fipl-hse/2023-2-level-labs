f105dc83eb809cd90f1ad610c81095e5c8ea7ead
artyomtugaryov <monadv@yandex.ru>
2023-11-02 19:02:52 +0300

    new lab docs

---
+++ lab_3_generate_by_ngrams/start.py
@@ -0,0 +1,25 @@
+"""
+Natural Language Generation starter
+"""
+from pathlib import Path
+from typing import Optional
+
+
+def open_text_file(path_to_open: Path) -> Optional[str]:
+    """
+    Opens text file
+    :param path_to_open: path for file opening
+    :return: text of file
+    """
+
+
+def main() -> None:
+    """
+    Launches an implementation
+    """
+    result = None
+    assert result
+
+
+if __name__ == "__main__":
+    main()
