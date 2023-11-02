0e8bfa9ba0f9a2698d68894030963a210f6cc7ad
Vladays <tsvetk1@yandex.ru>
2023-11-02 20:41:15 +0300

    Merge remote-tracking branch 'origin/main'

--- lab_2_tokenize_by_bpe/main.py
+++ lab_2_tokenize_by_bpe/main.py
@@ -14,6 +14,22 @@ def prepare_word(
     :param end_of_word: a token that signifies the end of word
     :return: preprocessed word
     """
+    if not isinstance(raw_word, str):
+        return None
+    if not (isinstance(start_of_word, str))\
+            or (start_of_word) is None:
+        return None
+    if not (isinstance(end_of_word, str))\
+            or (end_of_word) is None:
+        return None
+
+    tokens_list = list(raw_word)
+
+    if start_of_word:
+        tokens_list.insert(0, start_of_word)
+    if end_of_word:
+        tokens_list.append(end_of_word)
+    return tuple(tokens_list)
 
 
 def collect_frequencies(
