b17cee9a136932eebd79ac6f890fbc5c792c5160
Damir-Khamidullin <hamidullindamir479@gmail.com>
2023-11-02 03:16:17 +0300

    hamidullindamir479@gmail.com

--- lab_2_tokenize_by_bpe/start.py
+++ lab_2_tokenize_by_bpe/start.py
@@ -1,10 +1,11 @@
 """
 BPE Tokenizer starter
 """
-
+import json
 from pathlib import Path
 
-from lab_2_tokenize_by_bpe.main import collect_frequencies, decode, get_vocabulary, train
+from lab_2_tokenize_by_bpe.main import (calculate_bleu, collect_frequencies,
+                                        decode, encode, get_vocabulary, train)
 
 
 def main() -> None:
@@ -18,6 +19,7 @@ def main() -> None:
         secret = text_file.read()
     word_frequencies = collect_frequencies(text, None, '</s>')
     massive_merge = train(word_frequencies, 100)
+    assert isinstance(massive_merge, dict)
     vocab = get_vocabulary(massive_merge, '<unk>')
     secret_prepared = []
     for num in secret.split():
@@ -25,6 +27,12 @@ def main() -> None:
     result = decode(secret_prepared, vocab, '</s>')
     print(result)
     assert result, "Encoding is not working"
+    with open(assets_path / 'for_translation_ru_raw.txt', 'r', encoding='utf-8') as text_file:
+        text_for_translation = text_file.read()
+    with open(assets_path / 'vocab.json', 'r', encoding='utf-8') as text_file:
+        vocabulary = json.load(text_file)
+    encoded_text_f_t = encode(text_for_translation, vocabulary, '\u2581', None, '<unk>')
+    print(encoded_text_f_t)
 
 
 if __name__ == "__main__":
