918133cd7f34e84fd4c6cc8dda85bf64674f0a66
Arina Savina <arinasavina330@gmail.com>
2023-11-02 20:14:33 +0300

    Laboratory work #2, Arina Savina - 22FPL1

--- lab_2_tokenize_by_bpe/start.py
+++ lab_2_tokenize_by_bpe/start.py
@@ -1,8 +1,10 @@
 """
 BPE Tokenizer starter
 """
+
 from pathlib import Path
 from lab_2_tokenize_by_bpe.main import get_vocabulary, decode, collect_frequencies, train
+
 
 def main() -> None:
     """
@@ -11,14 +13,15 @@ def main() -> None:
     assets_path = Path(__file__).parent / 'assets'
     with open(assets_path / 'text.txt', 'r', encoding='utf-8') as text_file:
         text = text_file.read()
-
-    with open(assets_path /'secrets' / 'secret_1', 'r', encoding='utf-8') as text_file:
+    with open(assets_path / 'secrets' / 'secret_1', 'r', encoding='utf-8') as text_file:
         encoded_text = text_file.read().split()
         encoded_list = [int(el) for el in encoded_text]
-        word_frequencies = train(collect_frequencies(text, start_of_word=None, end_of_word='</s>'), 100)
-        vocabulary = get_vocabulary(word_frequencies,'<unk>')
-    result = decode(encoded_list, vocabulary,'</s>')
-    assert result, "Encoding is not working"
+        word_frequencies = train(collect_frequencies(text, None, '</s>'), 100)
+        if word_frequencies:
+            vocabulary = get_vocabulary(word_frequencies, '<unk>')
+            result = decode(encoded_list, vocabulary, '</s>')
+            print(result)
+            assert result, "Encoding is not working"
 
 
 if __name__ == "__main__":
