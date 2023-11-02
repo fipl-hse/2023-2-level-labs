b92265f83aea3e05662f05ad42247e071f4a296d
Arina Savina <arinasavina330@gmail.com>
2023-11-02 20:20:07 +0300

    Laboratory work #2, Arina Savina - 22FPL1

--- lab_2_tokenize_by_bpe/start.py
+++ lab_2_tokenize_by_bpe/start.py
@@ -3,7 +3,7 @@ BPE Tokenizer starter
 """
 
 from pathlib import Path
-from lab_2_tokenize_by_bpe.main import get_vocabulary, decode, collect_frequencies, train
+from lab_2_tokenize_by_bpe.main import collect_frequencies, decode, get_vocabulary, train
 
 
 def main() -> None:
@@ -13,7 +13,7 @@ def main() -> None:
     assets_path = Path(__file__).parent / 'assets'
     with open(assets_path / 'text.txt', 'r', encoding='utf-8') as text_file:
         text = text_file.read()
-    with open(assets_path / 'secrets' / 'secret_1', 'r', encoding='utf-8') as text_file:
+    with open(assets_path / 'secrets' / 'secret_3.txt', 'r', encoding='utf-8') as text_file:
         encoded_text = text_file.read().split()
         encoded_list = [int(el) for el in encoded_text]
         word_frequencies = train(collect_frequencies(text, None, '</s>'), 100)
