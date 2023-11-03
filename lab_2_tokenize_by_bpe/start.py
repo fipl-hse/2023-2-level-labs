b431d5fdeb37696fe3252e4d94da03d5f2ea45a1
Соня <saonofrienko@edu.hse.ru>
2023-11-03 05:10:42 +0300

    changes in start.py

--- lab_2_tokenize_by_bpe/start.py
+++ lab_2_tokenize_by_bpe/start.py
@@ -3,7 +3,7 @@ BPE Tokenizer starter
 """
 from pathlib import Path
 
-from lab_2_tokenize_by_bpe.main import collect_frequencies, train, get_vocabulary, decode
+import lab_2_tokenize_by_bpe.main as main_bpe
 
 
 def main() -> None:
@@ -13,16 +13,16 @@ def main() -> None:
     assets_path = Path(__file__).parent / 'assets'
     with open(assets_path / 'text.txt', 'r', encoding='utf-8') as text_file:
         text = text_file.read()
-    word_frequencies = collect_frequencies(text, None, '</s>')
-    word_frequencies = train(word_frequencies, 100)
+    word_frequencies = main_bpe.collect_frequencies(text, None, '</s>')
+    word_frequencies = main_bpe.train(word_frequencies, 100)
     secret_list = []
     vocabulary = {}
     if word_frequencies is not None:
         with open(assets_path / 'secrets/secret_1.txt', 'r', encoding='utf-8') as file:
             secret = file.read()
-        vocabulary = get_vocabulary(word_frequencies, '<unk>')
+        vocabulary = main_bpe.get_vocabulary(word_frequencies, '<unk>')
         secret_list = [int(num) for num in secret.split(' ')]
-    result = decode(secret_list, vocabulary, '</s>')
+    result = main_bpe.decode(secret_list, vocabulary, '</s>')
     print(result)
 
     assert result, "Encoding is not working"
