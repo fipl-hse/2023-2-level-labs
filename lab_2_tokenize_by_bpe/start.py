65246bbd856e31caef428ee602d2fc591848d388
artyomtugaryov <artyomtugaryov@users.noreply.github.com>
2023-11-03 17:28:18 +0300

    checkout labs from the origin repository

--- lab_2_tokenize_by_bpe/start.py
+++ lab_2_tokenize_by_bpe/start.py
@@ -1,7 +1,6 @@
 """
 BPE Tokenizer starter
 """
-import json
 from pathlib import Path
 
 from lab_2_tokenize_by_bpe.main import prepare_word, collect_frequencies, count_tokens_pairs, merge_tokens
@@ -14,16 +13,6 @@ def main() -> None:
     assets_path = Path(__file__).parent / 'assets'
     with open(assets_path / 'text.txt', 'r', encoding='utf-8') as text_file:
         text = text_file.read()
-    with open(assets_path / 'secrets/secret_2.txt', 'r', encoding='utf-8') as text_file:
-        encoded_secret = text_file.read()
-    dict_frequencies = collect_frequencies(text, None, '</s>')
-    merged_tokens = train(dict_frequencies, 100)
-    if merged_tokens:
-        vocabulary = get_vocabulary(merged_tokens, '<unk>')
-        secret = [int(num) for num in encoded_secret.split()]
-        result = decode(secret, vocabulary, '</s>')
-        print(result)
-        assert result, "Encoding is not working"
 
     print(prepare_word('father', '</b>', '</s>'))
     print(collect_frequencies(text, None, '</s>'))
