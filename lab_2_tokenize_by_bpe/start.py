e680ce38993f92dbff17be0cc6c0e25419d074a9
artyomtugaryov <artyomtugaryov@users.noreply.github.com>
2023-11-03 17:24:32 +0300

    checkout labs from the origin repository

--- lab_2_tokenize_by_bpe/start.py
+++ lab_2_tokenize_by_bpe/start.py
@@ -4,7 +4,8 @@ BPE Tokenizer starter
 import json
 from pathlib import Path
 
-from lab_2_tokenize_by_bpe.main import calculate_bleu, decode, encode
+from lab_2_tokenize_by_bpe.main import (calculate_bleu, collect_frequencies, decode, encode,
+                                        get_vocabulary, train)
 
 
 def main() -> None:
@@ -14,29 +15,37 @@ def main() -> None:
     assets_path = Path(__file__).parent / 'assets'
     with open(assets_path / 'text.txt', 'r', encoding='utf-8') as text_file:
         text = text_file.read()
+    with open(assets_path / 'secrets/secret_2.txt', 'r', encoding='utf-8') as text_file:
+        encoded_secret = text_file.read()
+    dict_frequencies = collect_frequencies(text, None, '</s>')
+    merged_tokens = train(dict_frequencies, 100)
+    if merged_tokens:
+        vocabulary = get_vocabulary(merged_tokens, '<unk>')
+        secret = [int(num) for num in encoded_secret.split()]
+        result = decode(secret, vocabulary, '</s>')
+        print(result)
+        assert result, "Encoding is not working"
+
+    with open(assets_path / 'for_translation_ru_raw.txt', 'r', encoding='utf-8') as file:
+        predicted = file.read()
     with open(assets_path / 'vocab.json', 'r', encoding='utf-8') as file:
         vocabulary = json.load(file)
-    with open(assets_path / 'for_translation_ru_raw.txt', 'r', encoding='utf-8') as file:
-        ru_raw = file.read()
     with open(assets_path / 'for_translation_ru_encoded.txt', 'r', encoding='utf-8') as file:
-        ru_encoded = file.read()
+        actual = file.read()
 
-    encode_pred = encode(ru_raw, vocabulary, '\u2581', None, '<unk>')
-    correct_tokens = [token for token in encode_pred if token in map(int, ru_encoded.split())]
-    if correct_tokens and encode_pred:
-        print((f"Файл закодирован правильно на "
-               f"{(len(list(correct_tokens)) / len(list(encode_pred))*100)}%"))
+    if [int(token) for token in actual.split()] == encode(
+            predicted, vocabulary, '\u2581', None, '<unk>'):
+        print("Encoding is successful!")
 
     with open(assets_path / 'for_translation_en_encoded.txt', 'r', encoding='utf-8') as file:
         encoded_en = file.read()
     with open(assets_path / 'for_translation_en_raw.txt', 'r', encoding='utf-8') as file:
-        en_raw = file.read()
+        decoded_en = file.read()
 
-    decoded_text = decode([int(num) for num in encoded_en.split()], vocabulary, None)
-    decoded_text = decoded_text.replace('\u2581', ' ')
-    result = calculate_bleu(decoded_text, en_raw)
-    print(f'BLUE = {result}')
-    assert result, "Encoding is not working"
+    decoded = decode([int(num) for num in encoded_en.split()], vocabulary, None)
+    decoded = decoded.replace('\u2581', ' ')
+
+    print(calculate_bleu(decoded, decoded_en))
 
 
 if __name__ == "__main__":
