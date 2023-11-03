50333050f713a9981c30bd790bc48154d03fc3f0
Соня <saonofrienko@edu.hse.ru>
2023-11-03 05:46:18 +0300

    some changes

--- lab_2_tokenize_by_bpe/main.py
+++ lab_2_tokenize_by_bpe/main.py
@@ -147,12 +147,13 @@ def get_vocabulary(
     if word_frequencies is None:
         return None
     new_dict = {}
-    list_of_tokens = set(unknown_token)
+    list_of_tokens = set()
     for word in word_frequencies:
         for token in word:
             list_of_tokens.add(token)
             for letter in token:
                 list_of_tokens.add(letter)
+    list_of_tokens.add(unknown_token)
     sorted_list_of_tokens = sorted(list_of_tokens)
     sorted_list_of_tokens_2 = sorted(sorted_list_of_tokens, key=len, reverse=True)
     for i, token in enumerate(sorted_list_of_tokens_2):
