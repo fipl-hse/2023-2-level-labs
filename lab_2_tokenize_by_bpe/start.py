992546288af8dbd38d1f5dc531a23ba0628ce692
artyomtugaryov <artyomtugaryov@users.noreply.github.com>
2023-11-03 17:16:21 +0300

    checkout labs from the origin repository

--- lab_2_tokenize_by_bpe/start.py
+++ lab_2_tokenize_by_bpe/start.py
@@ -15,37 +15,27 @@ def main() -> None:
     assets_path = Path(__file__).parent / 'assets'
     with open(assets_path / 'text.txt', 'r', encoding='utf-8') as text_file:
         text = text_file.read()
-    with open(assets_path / 'secrets/secret_2.txt', 'r', encoding='utf-8') as text_file:
-        encoded_secret = text_file.read()
-    dict_frequencies = collect_frequencies(text, None, '</s>')
-    merged_tokens = train(dict_frequencies, 100)
-    if merged_tokens:
-        vocabulary = get_vocabulary(merged_tokens, '<unk>')
-        secret = [int(num) for num in encoded_secret.split()]
-        result = decode(secret, vocabulary, '</s>')
-        print(result)
-        assert result, "Encoding is not working"
-
-    with open(assets_path / 'for_translation_ru_raw.txt', 'r', encoding='utf-8') as file:
-        predicted = file.read()
-    with open(assets_path / 'vocab.json', 'r', encoding='utf-8') as file:
-        vocabulary = json.load(file)
-    with open(assets_path / 'for_translation_ru_encoded.txt', 'r', encoding='utf-8') as file:
-        actual = file.read()
-
-    if [int(token) for token in actual.split()] == encode(
-            predicted, vocabulary, '\u2581', None, '<unk>'):
-        print("Encoding is successful!")
-
-    with open(assets_path / 'for_translation_en_encoded.txt', 'r', encoding='utf-8') as file:
-        encoded_en = file.read()
-    with open(assets_path / 'for_translation_en_raw.txt', 'r', encoding='utf-8') as file:
-        decoded_en = file.read()
-
-    decoded = decode([int(num) for num in encoded_en.split()], vocabulary, None)
-    decoded = decoded.replace('\u2581', ' ')
-
-    print(calculate_bleu(decoded, decoded_en))
+    with open(assets_path / 'secrets/secret_1.txt', 'r', encoding='utf-8') as text_file:
+        secret = text_file.read()
+    massive_merge = train(collect_frequencies(text, None, '</s>'), 100)
+    assert isinstance(massive_merge, dict)
+    vocab = get_vocabulary(massive_merge, '<unk>')
+    result = decode([int(num) for num in secret.split()], vocab, '</s>')
+    print(result)
+    assert result, "Encoding is not working"
+    with open(assets_path / 'for_translation_ru_raw.txt', 'r', encoding='utf-8') as text_file:
+        text_for_translation = text_file.read()
+    with open(assets_path / 'vocab.json', 'r', encoding='utf-8') as text_file:
+        vocabulary = json.load(text_file)
+    with open(assets_path / 'for_translation_en_encoded.txt', 'r', encoding='utf-8') as text_file:
+        encoded_en_text = text_file.read()
+    with open(assets_path / 'for_translation_en_raw.txt', 'r', encoding='utf-8') as text_file:
+        good_translation = text_file.read()
+    encoded_text_f_t = encode(text_for_translation, vocabulary, '\u2581', None, '<unk>')
+    decoded_en_text = decode([int(num) for num in encoded_en_text.split()], vocabulary, None)
+    decoded_en_text = decoded_en_text.replace('\u2581', ' ')
+    comparison = calculate_bleu(decoded_en_text, good_translation)
+    print(comparison)
 
 
 if __name__ == "__main__":
