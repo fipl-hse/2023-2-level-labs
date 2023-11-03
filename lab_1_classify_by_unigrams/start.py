8dddc2e555d34e0f1f9320fa88b2f1bd9b64f34c
artyomtugaryov <artyomtugaryov@users.noreply.github.com>
2023-11-03 17:13:31 +0300

    checkout labs from the origin repository

--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -2,8 +2,8 @@
 Language detection starter
 """
 
-
-from lab_1_classify_by_unigrams.main import create_language_profile, detect_language
+from lab_1_classify_by_unigrams.main import (collect_profiles, create_language_profile,
+                                             detect_language_advanced, print_report)
 
 
 def main() -> None:
@@ -17,17 +17,22 @@ def main() -> None:
     with open("assets/texts/unknown.txt", "r", encoding="utf-8") as file_to_read_unk:
         unknown_text = file_to_read_unk.read()
 
-    unknown_prof = create_language_profile("unknown", unknown_text)
-    en_prof = create_language_profile("en", en_text)
-    de_prof = create_language_profile("de", de_text)
-    if (isinstance(unknown_prof, dict)
-            and isinstance(en_prof, dict)
-            and isinstance(de_prof, dict)):
-        result = detect_language(unknown_prof, en_prof, de_prof)
-    if result:
-        print(result)
-    assert result, "Detection result is None"
+    json_paths = ['assets/profiles/de.json',
+                  'assets/profiles/en.json',
+                  'assets/profiles/es.json',
+                  'assets/profiles/fr.json',
+                  'assets/profiles/it.json',
+                  'assets/profiles/ru.json',
+                  'assets/profiles/tr.json']
 
+    unknown = create_language_profile('unknown', unknown_text)
+    known = collect_profiles(json_paths)
+    if isinstance(unknown, dict) and isinstance(known, list):
+        result = detect_language_advanced(unknown, known)
+    if result:
+        print_report(result)
+
+    assert result, "Detection result is None"
 
 if __name__ == "__main__":
     main()
--- lab_2_tokenize_by_bpe/start.py
+++ lab_2_tokenize_by_bpe/start.py
@@ -1,11 +1,10 @@
 """
 BPE Tokenizer starter
 """
-import json
+
 from pathlib import Path
 
-from lab_2_tokenize_by_bpe.main import (calculate_bleu, collect_frequencies, decode, encode,
-                                        get_vocabulary, train)
+from lab_2_tokenize_by_bpe.main import collect_frequencies, decode, get_vocabulary, train
 
 
 def main() -> None:
@@ -15,37 +14,16 @@ def main() -> None:
     assets_path = Path(__file__).parent / 'assets'
     with open(assets_path / 'text.txt', 'r', encoding='utf-8') as text_file:
         text = text_file.read()
-    with open(assets_path / 'secrets/secret_2.txt', 'r', encoding='utf-8') as text_file:
-        encoded_secret = text_file.read()
-    dict_frequencies = collect_frequencies(text, None, '</s>')
-    merged_tokens = train(dict_frequencies, 100)
-    if merged_tokens:
-        vocabulary = get_vocabulary(merged_tokens, '<unk>')
-        secret = [int(num) for num in encoded_secret.split()]
-        result = decode(secret, vocabulary, '</s>')
-        print(result)
-        assert result, "Encoding is not working"
-
-    with open(assets_path / 'for_translation_ru_raw.txt', 'r', encoding='utf-8') as file:
-        predicted = file.read()
-    with open(assets_path / 'vocab.json', 'r', encoding='utf-8') as file:
-        vocabulary = json.load(file)
-    with open(assets_path / 'for_translation_ru_encoded.txt', 'r', encoding='utf-8') as file:
-        actual = file.read()
-
-    if [int(token) for token in actual.split()] == encode(
-            predicted, vocabulary, '\u2581', None, '<unk>'):
-        print("Encoding is successful!")
-
-    with open(assets_path / 'for_translation_en_encoded.txt', 'r', encoding='utf-8') as file:
-        encoded_en = file.read()
-    with open(assets_path / 'for_translation_en_raw.txt', 'r', encoding='utf-8') as file:
-        decoded_en = file.read()
-
-    decoded = decode([int(num) for num in encoded_en.split()], vocabulary, None)
-    decoded = decoded.replace('\u2581', ' ')
-
-    print(calculate_bleu(decoded, decoded_en))
+    with open(assets_path / 'secrets'/'secret_3.txt', 'r', encoding='utf-8') as text_file:
+        encoded_text = text_file.read()
+    dict_train = train(collect_frequencies(text, start_of_word=None, end_of_word='</s>'), 100)
+    if not dict_train:
+        return
+    vocabulary = get_vocabulary(dict_train, '<unk>')
+    encoded_list = [int(coded) for coded in encoded_text.split()]
+    result = decode(encoded_list, vocabulary, '</s>')
+    print(result)
+    assert result, "Encoding is not working"
 
 
 if __name__ == "__main__":
