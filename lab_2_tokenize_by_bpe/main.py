7d6e8367d7d9eb1b7358b78ba1669fceb7b0dcc4
mary <ma6ylabuk@gmail.com>
2023-11-10 11:03:07 +0300

    lab_2_tokenize_by_bpe Bukina

--- lab_2_tokenize_by_bpe/main.py
+++ lab_2_tokenize_by_bpe/main.py
@@ -14,8 +14,9 @@ def prepare_word(
     :param end_of_word: a token that signifies the end of word
     :return: preprocessed word
     """
-    if not isinstance(raw_word, str) or not isinstance(end_of_word, str | None) or not isinstance(start_of_word,
-                                                                                                  str | None):
+    if (not isinstance(raw_word, str) or
+            not (isinstance(start_of_word, str) or start_of_word is None) or
+            not (isinstance(end_of_word, str) or end_of_word is None)):
         return None
 
     raw_tokens = list(raw_word)
