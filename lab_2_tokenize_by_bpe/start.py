ee85b35d0b009b6314f65c35cbb28a144883e606
Vladays <tsvetk1@yandex.ru>
2023-11-10 11:02:31 +0300

    Merge remote-tracking branch 'origin/main'

--- lab_2_tokenize_by_bpe/start.py
+++ lab_2_tokenize_by_bpe/start.py
@@ -3,8 +3,7 @@ BPE Tokenizer starter
 """
 from pathlib import Path
 
-from lab_2_tokenize_by_bpe.main import collect_frequencies
-
+from lab_2_tokenize_by_bpe.main import collect_frequencies, train, get_vocabulary, decode
 
 def main() -> None:
     """
@@ -14,9 +13,18 @@ def main() -> None:
     with open(assets_path / 'text.txt', 'r', encoding='utf-8') as text_file:
         text = text_file.read()
 
-    result = collect_frequencies(text, start_of_word = None, end_of_word = '</s>')
+    with open(assets_path / 'secrets/secret_4.txt', 'r', encoding='utf-8') as text_file:
+        encoded = text_file.read()
+    freq_dict = collect_frequencies(text, None, '</s>')
+    merged_tokens = train(freq_dict, 100)
 
-    assert result, "Encoding is not working"
+    if merged_tokens:
+        vocab = get_vocabulary(merged_tokens, '<unk>')
+        secret = [int(num) for num in encoded.split()]
+        result = decode(secret, vocab, '</s>')
+        print(result)
+
+        assert result, "Encoding is not working"
 
 
 if __name__ == "__main__":
