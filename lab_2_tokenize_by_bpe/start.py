1cc77ac6a1357d0c12f17499cb4ec33ee9e63fcb
mary <ma6ylabuk@gmail.com>
2023-11-10 11:58:21 +0300

    lab_2_tokenize_by_bpe Bukina

--- lab_2_tokenize_by_bpe/start.py
+++ lab_2_tokenize_by_bpe/start.py
@@ -3,7 +3,7 @@ BPE Tokenizer starter
 """
 from pathlib import Path
 
-from lab_2_tokenize_by_bpe.main import collect_frequencies, count_tokens_pairs, merge_tokens, prepare_word
+import lab_2_tokenize_by_bpe.main as main_py
 
 
 def main() -> None:
@@ -14,10 +14,10 @@ def main() -> None:
     with open(assets_path / 'text.txt', 'r', encoding='utf-8') as text_file:
         text = text_file.read()
 
-    print(prepare_word('father', '</b>', '</s>'))
-    print(collect_frequencies(text, None, '</s>'))
-    print(count_tokens_pairs(collect_frequencies(text, None, '</s>')))
-    print(merge_tokens(collect_frequencies(text, None, '</s>'), (',', '</s>')))
+    print(main_py.prepare_word('father', '</b>', '</s>'))
+    print(main_py.collect_frequencies(text, None, '</s>'))
+    print(main_py.count_tokens_pairs(main_py.collect_frequencies(text, None, '</s>')))
+    print(main_py.merge_tokens(main_py.collect_frequencies(text, None, '</s>'), (',', '</s>')))
     result = 1
     assert result, "Encoding is not working"
 
