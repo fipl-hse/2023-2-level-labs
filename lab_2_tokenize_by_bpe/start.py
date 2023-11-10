7c70694ab31abd5458b462454a26fd7da4bee96b
слава угольников <viacheslav.ugolnickoff@gmail.com>
2023-11-10 09:50:59 +0300

    Commit:P
    Signed-off-by: слава угольников <viacheslav.ugolnickoff@gmail.com>

--- lab_2_tokenize_by_bpe/start.py
+++ lab_2_tokenize_by_bpe/start.py
@@ -2,7 +2,8 @@
 BPE Tokenizer starter
 """
 from pathlib import Path
-from lab_2_tokenize_by_bpe.main import collect_frequencies, train
+
+import lab_2_tokenize_by_bpe.main as main_bpe
 
 
 def main() -> None:
@@ -12,8 +13,8 @@ def main() -> None:
     assets_path = Path(__file__).parent / 'assets'
     with open(assets_path / 'text.txt', 'r', encoding='utf-8') as text_file:
         text = text_file.read()
-        result = collect_frequencies(text, None, '</s>')
-        train_result = train(result, 100)
+    result = (main_bpe.collect_frequencies(text, None, '</s>'))
+    train_result = (main_bpe.train(result, 100))
 
     if result is None:
         assert result, "Encoding is not working"
