d9981fc4ef34b028917327cb59ad1ec08fe36dff
egor-solo <egor-soloviev@mail.ru>
2023-11-16 21:48:07 +0300

    commit

--- lab_2_tokenize_by_bpe/start.py
+++ lab_2_tokenize_by_bpe/start.py
@@ -3,7 +3,7 @@ BPE Tokenizer starter
 """
 from pathlib import Path
 
-from lab_2_tokenize_by_bpe.main import collect_frequencies, train
+from lab_2_tokenize_by_bpe.main import collect_frequencies, train, decode, get_vocabulary
 
 
 def main() -> None:
@@ -13,10 +13,15 @@ def main() -> None:
     assets_path = Path(__file__).parent / 'assets'
     with open(assets_path / 'text.txt', 'r', encoding='utf-8') as text_file:
         text = text_file.read()
-    word_freq = collect_frequencies(text, None, '</s>')
-    result = train(word_freq, 100)
+    with open(assets_path / 'secrets'/'secret_3.txt', 'r', encoding='utf-8') as text_file:
+        encoded_text = text_file.read()
+    dict_train = train(collect_frequencies(text, start_of_word=None, end_of_word='</s>'), 100)
+    if not dict_train:
+        return
+    vocabulary = get_vocabulary(dict_train, '<unk>')
+    encoded_list = [int(coded) for coded in encoded_text.split()]
+    result = decode(encoded_list, vocabulary, '</s>')
     print(result)
-    assert result, "Encoding is not working"
 
 
 if __name__ == "__main__":
