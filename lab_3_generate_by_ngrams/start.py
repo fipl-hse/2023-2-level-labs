e36e388df98931e6d750c6b6288149de50173f0e
AlisaKarpova <aliskakarpowa@yandex.ru>
2023-11-19 01:15:47 +0300

    start.py

--- lab_3_generate_by_ngrams/start.py
+++ lab_3_generate_by_ngrams/start.py
@@ -1,8 +1,8 @@
 """
 Generation by NGrams starter
 """
-
-from lab_3_generate_by_ngrams.main import TextProcessor, NGramLanguageModel, GreedyTextGenerator
+from lab_3_generate_by_ngrams.main import (GreedyTextGenerator, NGramLanguageModel,
+                                           TextProcessor)
 
 
 def main() -> None:
