b1eb2a3a1899c716f251acc2bfebca6541ece2fb
Your Name <tusyakulak.4@gmail.com>
2023-11-19 15:38:04 +0300

    start tests fixed

--- lab_3_generate_by_ngrams/start.py
+++ lab_3_generate_by_ngrams/start.py
@@ -1,7 +1,7 @@
 """
 Generation by NGrams starter
 """
-from main import NGramLanguageModel, TextProcessor
+from main import GreedyTextGenerator, NGramLanguageModel, TextProcessor
 
 
 def main() -> None:
@@ -12,16 +12,21 @@ def main() -> None:
     """
     with open("./assets/Harry_Potter.txt", "r", encoding="utf-8") as text_file:
         text = text_file.read()
+
     processor = TextProcessor(end_of_word_token='_')
     encoded = processor.encode(text)
-    decoded = str(processor.decode(encoded))
-    result = decoded
+    result = processor.decode(encoded)
     print(result[:100])
 
     model = NGramLanguageModel(encoded[:100], n_gram_size=3)
-    result = model.build()
-    print(result)
+    print(model.build())
+
+    model2 = NGramLanguageModel(encoded, 7)
+    greedy_text_generator = GreedyTextGenerator(model2, processor)
+    print(greedy_text_generator.run(51, 'Vernon'))
+
     assert result
+
 
 if __name__ == "__main__":
     main()
