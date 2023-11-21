59785cfb545eff0ec248f7349678344ce1acef34
kobyakova polina <polinagg.pk@gmail.com>
2023-11-21 18:14:18 +0300

    lab3 fixing problems

--- lab_3_generate_by_ngrams/start.py
+++ lab_3_generate_by_ngrams/start.py
@@ -1,7 +1,8 @@
 """
 Generation by NGrams starter
 """
-from lab_3_generate_by_ngrams.main import TextProcessor, NGramLanguageModel, GreedyTextGenerator
+import lab_3_generate_by_ngrams.main as main_py
+#from lab_3_generate_by_ngrams.main import TextProcessor, NGramLanguageModel, GreedyTextGenerator
 
 def main() -> None:
     """
@@ -12,18 +13,18 @@ def main() -> None:
     with open("./assets/Harry_Potter.txt", "r", encoding="utf-8") as text_file:
         text = text_file.read()
     result = None
-    processor = TextProcessor('_')
+    processor = main_py.TextProcessor('_')
     encoded_text = processor.encode(text)
     result = processor.decode(encoded_text)
     print(encoded_text)
     print(result)
 
-    lang_model = NGramLanguageModel(encoded_text, 2)
+    lang_model = main_py.NGramLanguageModel(encoded_text, 2)
     build = lang_model.build()
     print(build)
 
-    language_model = NGramLanguageModel(encoded_text, 7)
-    greedy_gen = GreedyTextGenerator(language_model, processor)
+    language_model = main_py.NGramLanguageModel(encoded_text, 7)
+    greedy_gen = main_py.GreedyTextGenerator(language_model, processor)
     generated_text = greedy_gen.run(51, 'Vernon')
     print(generated_text)
     assert result
