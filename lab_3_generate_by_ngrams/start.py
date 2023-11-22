7bd9dc85ec0e9f3a6f13348260c203714457b7d0
elena <eiermal@edu.hse.ru>
2023-11-22 23:20:42 +0300

    mypy

--- lab_3_generate_by_ngrams/start.py
+++ lab_3_generate_by_ngrams/start.py
@@ -15,17 +15,20 @@ def main() -> None:
         text = text_file.read()
     story = TextProcessor("_")
     encoded = story.encode(text)
-    decoded_story = story.decode(encoded)
+    if not(isinstance(encoded, tuple) and encoded):
+        return
+
+    decoded_story = str(story.decode(encoded))
     result = decoded_story
+
     lang_model = NGramLanguageModel(encoded[:50], 3)
-    model = lang_model.build()
     model2 = NGramLanguageModel(encoded, 7)
     greedy = GreedyTextGenerator(model2, story)
     print(greedy.run(51, "Vernon"))
+
     beam_search = BeamSearchTextGenerator(model2, story, 7)
     print(beam_search.run('Vernon', 56))
     assert result
-
 
 if __name__ == "__main__":
     main()
