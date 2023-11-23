3d08087104ff25b4423b17efeae51622a2cf446a
egor-solo <dotabezgranic@mail.ru>
2023-11-23 23:52:02 +0300

    commit

--- lab_3_generate_by_ngrams/start.py
+++ lab_3_generate_by_ngrams/start.py
@@ -1,7 +1,7 @@
 """
 Generation by NGrams starter
 """
-from main import (TextProcessor, NGramLanguageModel, GreedyTextGenerator)
+import lab_3_generate_by_ngrams.main as main_py
 
 
 def main() -> None:
@@ -12,17 +12,17 @@ def main() -> None:
     """
     with open("./assets/Harry_Potter.txt", "r", encoding="utf-8") as text_file:
         text = text_file.read()
-    text_processor = TextProcessor('_')
+    text_processor = main_py.TextProcessor('_')
     encoded_text = text_processor.encode(text)
     if isinstance(encoded_text, tuple) and encoded_text:
         result = text_processor.decode(encoded_text)[:100]
         print(result)
 
-        model = NGramLanguageModel(encoded_text[:100], n_gram_size=3)
+        model = main_py.NGramLanguageModel(encoded_text[:100], n_gram_size=3)
         print(model.build())
 
-        model2 = NGramLanguageModel(encoded_text, 7)
-        greedy_text_generator = GreedyTextGenerator(model2, text_processor)
+        model2 = main_py.NGramLanguageModel(encoded_text, 7)
+        greedy_text_generator = main_py.GreedyTextGenerator(model2, text_processor)
         print(greedy_text_generator.run(51, 'Vernon'))
 
         assert result
