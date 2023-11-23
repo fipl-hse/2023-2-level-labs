eb4e0b4d656b969e4e4a7cce5b6f7f0e71a26545
artyom6k <artyom.krivczov@mail.ru>
2023-11-24 01:11:48 +0300

    Laboratory work #3, Krivtsov Artyom - 22FPL2

--- lab_3_generate_by_ngrams/start.py
+++ lab_3_generate_by_ngrams/start.py
@@ -11,7 +11,7 @@ def main() -> None:
     """
     with open("./assets/Harry_Potter.txt", "r", encoding="utf-8") as text_file:
         text = text_file.read()
-    text_processor = TextProcessor(end_of_word_token='_')
+    text_processor = TextProcessor('_')
     encoded = text_processor.encode(text)
     decoded = text_processor.decode(encoded)
     result = decoded
