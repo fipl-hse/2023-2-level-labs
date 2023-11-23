6aaa21b853ff60c1574ca63b5fed7ceea8583084
RostislavHmelevski <warian11031978@gmail.com>
2023-11-23 21:53:58 +0300

    "Added Tokenize Method"

--- lab_1_classify_by_unigrams/main.py
+++ lab_1_classify_by_unigrams/main.py
@@ -5,12 +5,6 @@ Language detection
 
 
 def tokenize(text: str) -> list[str] | None:
-    """
-    Splits a text into tokens, converts the tokens into lowercase,
-    removes punctuation, digits and other symbols
-    :param text: a text
-    :return: a list of lower-cased tokens without punctuation
-    """
     text = text.lower()
     text = text.replace(' ', '')
     text = text.replace('\n', 'replace')
