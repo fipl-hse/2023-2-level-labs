c4327207e84865eb4300b1c01e9188e40bb637c7
Vladays <tsvetk1@yandex.ru>
2023-11-24 12:20:02 +0300

    Merge remote-tracking branch 'origin/main'

--- lab_3_generate_by_ngrams/start.py
+++ lab_3_generate_by_ngrams/start.py
@@ -1,17 +1,28 @@
 """
 Generation by NGrams starter
 """
+from lab_3_generate_by_ngrams.main import GreedyTextGenerator, NGramLanguageModel, TextProcessor
 
 
 def main() -> None:
     """
-    Launches an implementation.
-
-    In any case returns, None is returned
+    Launches an implementation
     """
     with open("./assets/Harry_Potter.txt", "r", encoding="utf-8") as text_file:
         text = text_file.read()
-    result = None
+    text_processor = TextProcessor('_')
+    encoded = text_processor.encode(text)
+    decoded = text_processor.decode(encoded)
+    result = decoded
+    print(result)
+
+    n_gram_language_model = NGramLanguageModel(encoded[:100], 7)
+    print(n_gram_language_model.build())
+    greedy_text_generator = GreedyTextGenerator(n_gram_language_model, text_processor)
+    generated_text = greedy_text_generator.run(51, 'Vernon')
+    result = generated_text
+
+    print(generated_text)
     assert result
 
 
