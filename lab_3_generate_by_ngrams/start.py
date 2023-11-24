a7b448e5f671b7c3f27be93a703dbf30b53d74eb
Baronofff <Baronov.ev@gmail.com>
2023-11-24 05:36:47 +0300

    Laboratory work #3, Evgeniy Baronov - 22FPL2

--- lab_3_generate_by_ngrams/start.py
+++ lab_3_generate_by_ngrams/start.py
@@ -1,7 +1,7 @@
 """
 Generation by NGrams starter
 """
-
+from lab_3_generate_by_ngrams.main import TextProcessor
 
 def main() -> None:
     """
@@ -12,8 +12,11 @@ def main() -> None:
     with open("./assets/Harry_Potter.txt", "r", encoding="utf-8") as text_file:
         text = text_file.read()
     result = None
-    assert result
-
+    text_processed = TextProcessor('_')
+    encoded_text = text_processed.encode(text)
+    print(encoded_text)
+    decoded_text = text_processed.decode(encoded_text)
+    print(decoded_text)
 
 if __name__ == "__main__":
     main()
