0cabc387273228a62ea23b29816cf374ebd62fdd
Vladays <tsvetk1@yandex.ru>
2023-11-24 12:02:24 +0300

    Merge remote-tracking branch 'origin/main'

--- lab_2_tokenize_by_bpe/start.py
+++ lab_2_tokenize_by_bpe/start.py
@@ -1,47 +1,32 @@
 """
 BPE Tokenizer starter
 """
-from lab_3_generate_by_ngrams.main import (GreedyTextGenerator,
-                                           NGramLanguageModel, TextProcessor)
+import json
+from pathlib import Path
+
+from lab_3_generate_by_ngrams.main import GreedyTextGenerator, NGramLanguageModel, TextProcessor
+
 
 def main() -> None:
     """
     Launches an implementation
     """
-    assets_path = Path(__file__).parent / 'assets'
-    with open(assets_path / 'text.txt', 'r', encoding='utf-8') as text_file:
+    with open("./assets/Harry_Potter.txt", "r", encoding="utf-8") as text_file:
         text = text_file.read()
-    with open(assets_path / 'secrets/secret_2.txt', 'r', encoding='utf-8') as text_file:
-        encoded_secret = text_file.read()
-    dict_frequencies = collect_frequencies(text, None, '</s>')
-    merged_tokens = train(dict_frequencies, 100)
-    if merged_tokens:
-        vocabulary = get_vocabulary(merged_tokens, '<unk>')
-        secret = [int(num) for num in encoded_secret.split()]
-        result = decode(secret, vocabulary, '</s>')
-        print(result)
-        assert result, "Encoding is not working"
+    text_processor = TextProcessor('_')
+    encoded = text_processor.encode(text)
+    decoded = text_processor.decode(encoded)
+    result = decoded
+    print(result)
 
-    with open(assets_path / 'for_translation_ru_raw.txt', 'r', encoding='utf-8') as file:
-        predicted = file.read()
-    with open(assets_path / 'vocab.json', 'r', encoding='utf-8') as file:
-        vocabulary = json.load(file)
-    with open(assets_path / 'for_translation_ru_encoded.txt', 'r', encoding='utf-8') as file:
-        actual = file.read()
+    n_gram_language_model = NGramLanguageModel(encoded[:100], 7)
+    print(n_gram_language_model.build())
+    greedy_text_generator = GreedyTextGenerator(n_gram_language_model, text_processor)
+    generated_text = greedy_text_generator.run(51, 'Vernon')
+    result = generated_text
 
-    if [int(token) for token in actual.split()] == encode(
-            predicted, vocabulary, '\u2581', None, '<unk>'):
-        print("Encoding is successful!")
-
-    with open(assets_path / 'for_translation_en_encoded.txt', 'r', encoding='utf-8') as file:
-        encoded_en = file.read()
-    with open(assets_path / 'for_translation_en_raw.txt', 'r', encoding='utf-8') as file:
-        decoded_en = file.read()
-
-    decoded = decode([int(num) for num in encoded_en.split()], vocabulary, None)
-    decoded = decoded.replace('\u2581', ' ')
-
-    print(calculate_bleu(decoded, decoded_en))
+    print(generated_text)
+    assert result
 
 
 if __name__ == "__main__":
