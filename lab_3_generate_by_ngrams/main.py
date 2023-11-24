82b66a33c4f5219dfcc3c5c7696d2cde0849aa4c
sofianurtdinova <asnurtdinova2@gmail.com>
2023-11-24 04:41:26 +0300

    Laboratory work #3, Sofia Nurtdinova - 22FPL2

--- lab_3_generate_by_ngrams/main.py
+++ lab_3_generate_by_ngrams/main.py
@@ -474,12 +474,11 @@ class BeamSearcher:
                 and len(next_tokens) <= self._beam_width
                 and sequence in sequence_candidates):
             return None
-        new_sequence_candidates = sequence_candidates.copy()
         for token in next_tokens:
-            new_sequence = sequence + (token[0],)
-            new_sequence_candidates[new_sequence] = sequence_candidates[sequence] - math.log(token[1])
-        new_sequence_candidates.pop(sequence)
-        return new_sequence_candidates
+            sequence_candidates.update({sequence+(token[0],): sequence_candidates[sequence] - math.log(
+                token[1])})
+        sequence_candidates.pop(sequence)
+        return sequence_candidates
 
     def prune_sequence_candidates(
         self, sequence_candidates: dict[tuple[int, ...], float]
