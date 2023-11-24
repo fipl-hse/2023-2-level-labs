5022648893bdd8f51e49db9f0db72d70695fa43f
ogabekmatyoqubov <ogabekmatyoqubov577@gmail.com>
2023-11-24 12:30:44 +0300

    Laboratory work #3, Matyakubov Ogabek - 22FPL2

--- lab_3_generate_by_ngrams/start.py
+++ lab_3_generate_by_ngrams/start.py
@@ -12,9 +12,7 @@ def main() -> None:
     """
     with open("./assets/Harry_Potter.txt", "r", encoding="utf-8") as text_file:
         text = text_file.read()
-    result = None
-
-
+    text_processor = main_py.TextProcessor('_')
     encoded_text = text_processor.encode(text)
     decoded_text = text_processor.decode(encoded_text)
     print(decoded_text)
@@ -27,7 +25,7 @@ def main() -> None:
     result = generated_text
     print(generated_text)
 
-     assert result
+    assert result
 
 
 
