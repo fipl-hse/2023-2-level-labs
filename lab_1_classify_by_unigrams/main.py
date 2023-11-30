e799f2cd392cfcf4f1d78ddb54ea4fac2e13065f
ulyanaabr <uliana.abramova@bk.ru>
2023-11-30 11:59:45 +0300

    lab1

--- lab_1_classify_by_unigrams/main.py
+++ lab_1_classify_by_unigrams/main.py
@@ -21,6 +21,16 @@ def tokenize(text: str) -> list[str] | None:
 
 
     def calculate_frequencies(tokens: list[str] | None) -> dict[str, float] | None:
+        """
+        Calculates frequencies of given tokens
+        :param tokens: a list of tokens
+        :return: a dictionary with frequencies
+        """
+        if not isinstance(tokens, list):
+            return None
+        if not all(isinstance(token, str) for token in tokens):
+            return None
+
         frequency = {}
         for i in tokens:
             frequency[i] = tokens.count(i) / len(tokens)
@@ -28,25 +38,21 @@ def tokenize(text: str) -> list[str] | None:
 
 
     def create_language_profile(language: str, text: str) -> dict[str, str | dict[str, float]] | None:
+        """
+        Creates a language profile
+        :param language: a language
+        :param text: a text
+        :return: a dictionary with two keys – name, freq
+        """
+        if not isinstance(language, str):
+            return None
+        if not isinstance(text, str):
+            return None
 
         tokenized = tokenize(text)
         frequencies = calculate_frequencies(tokenized)
         profile = dict({"name": language, "freq": frequencies})
         return profile
-
-
-
-
-    """
-
-
- def create_language_profile(language: str, text: str) -> dict[str, str | dict[str, float]] | None:
- 
-    Creates a language profile
-    :param language: a language
-    :param text: a text
-    :return: a dictionary with two keys – name, freq
-    """
 
 
 def calculate_mse(predicted: list, actual: list) -> float | None:
--- lab_1_classify_by_unigrams/start.py
+++ lab_1_classify_by_unigrams/start.py
@@ -1,7 +1,7 @@
 """
 Language detection starter
 """
-import tokenize 
+from lab_1_classify_by_unigrams.main import create_language_profile
 
 def main() -> None:
     """
@@ -13,8 +13,10 @@ def main() -> None:
         de_text = file_to_read_de.read()
     with open("assets/texts/unknown.txt", "r", encoding="utf-8") as file_to_read_unk:
         unknown_text = file_to_read_unk.read()
-   result = None
-   assert result, "Detection result is None"
+
+    result = create_language_profile("en", en_text)
+    assert result, "Detection result is None"
+    print(result)
 
 
 if __name__ == "__main__":
