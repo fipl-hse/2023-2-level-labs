7210c6573df29d2e86a48841f15d71ca4ddb269b
alesiamashkovtseva <alesiamashkovtseva@gmail.com>
2023-11-30 21:38:35 +0300

    import fixing

--- lab_3_generate_by_ngrams/start.py
+++ lab_3_generate_by_ngrams/start.py
@@ -1,7 +1,8 @@
 """
 Generation by NGrams starter
 """
-from lab_3_generate_by_ngrams.main import (BeamSearchTextGenerator, NGramLanguageModel, TextProcessor)
+from lab_3_generate_by_ngrams.main import (BeamSearchTextGenerator, GreedyTextGenerator,
+                                           NGramLanguageModel, TextProcessor)
 
 
 def main() -> None:
@@ -12,21 +13,21 @@ def main() -> None:
     """
     with open("./assets/Harry_Potter.txt", "r", encoding="utf-8") as text_file:
         text = text_file.read()
-    text_processor = TextProcessor('_')
-    encoded_corpus = text_processor.encode(text)
-    if isinstance(encoded_corpus, tuple) and encoded_corpus:
-        # decoded_text = str(text_processor.decode(encoded_corpus))
 
-        # language_model = main_py.NGramLanguageModel(encoded_corpus, 3)
+    processor = TextProcessor(end_of_word_token='_')
+    encoded = processor.encode(text)
+    if isinstance(encoded, tuple) and encoded:
+        decoded = str(processor.decode(encoded))
+        result = decoded
 
-        language_model = NGramLanguageModel(encoded_corpus, 7)
-        # greedy_generator = main_py.GreedyTextGenerator(language_model, text_processor)
-        # generated_text = greedy_generator.run(51, 'Vernon')
+        ng_model = NGramLanguageModel(encoded, n_gram_size=3)
 
-        beam_search_generator = BeamSearchTextGenerator(language_model, text_processor, 7)
-        beam_search_generated_text = beam_search_generator.run("Vernon", 56)
+        model_6 = NGramLanguageModel(encoded, 7)
+        greedy_text_generator = GreedyTextGenerator(model_6, processor)
 
-        result = beam_search_generated_text
+        beam_search_generator = BeamSearchTextGenerator(model_6, processor, 7)
+        print(beam_search_generator.run('Vernon', 56))
+        result = beam_search_generator.run('Vernon', 56)
         assert result
 
 
