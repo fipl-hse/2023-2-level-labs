ed9b8b73e671803220bf02832abfe1e86202f1b8
Ratmir Akhmatzyanov <ratmirahmatzanov11102003@gmail.com>
2023-12-01 05:41:05 +0300

    finally done

--- lab_3_generate_by_ngrams/main.py
+++ lab_3_generate_by_ngrams/main.py
@@ -228,11 +228,11 @@ class TextProcessor:
 
         In case of corrupt input arguments, None is returned
         """
-        if not (isinstance(decoded_corpus, tuple) or decoded_corpus):
+        if not (isinstance(decoded_corpus, tuple) and decoded_corpus):
             return None
-        tokens = list(decoded_corpus)
-        text = ''.join(tokens).replace(self._end_of_word_token, ' ').capitalize().rstrip()
-        return f'{text}.'
+
+        return f"{''.join(decoded_corpus).replace(self._end_of_word_token, ' ').capitalize().rstrip()}."
+
 
 class NGramLanguageModel:
     """
