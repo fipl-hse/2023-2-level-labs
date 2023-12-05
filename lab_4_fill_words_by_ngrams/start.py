7cbd80f7a163a717c220484e9c277be85a72bee5
logunova-anastasia <lgnvssst@gmail.com>
2023-12-05 22:26:22 +0300

    style corrections...

--- lab_4_fill_words_by_ngrams/start.py
+++ lab_4_fill_words_by_ngrams/start.py
@@ -2,9 +2,9 @@
 Filling word by ngrams starter
 """
 # pylint:disable=too-many-locals,unused-import
-from lab_3_generate_by_ngrams.main import BeamSearchTextGenerator, GreedyTextGenerator
-from lab_4_fill_words_by_ngrams.main import (GeneratorTypes, NGramLanguageModel,
-                                             QualityChecker, TopPGenerator, WordProcessor)
+from lab_3_generate_by_ngrams.main import BeamSearchTextGenerator, GreedyTextGenerator, NGramLanguageModel
+from lab_4_fill_words_by_ngrams.main import (GeneratorTypes, QualityChecker,
+                                             TopPGenerator, WordProcessor)
 
 
 def main() -> None:
