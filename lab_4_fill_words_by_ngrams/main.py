b4e53562d492ca11ac3b176d8f314b788877cdbb
AlisaKarpova <aliskakarpowa@yandex.ru>
2023-12-06 10:47:41 +0300

    step 4

--- lab_4_fill_words_by_ngrams/main.py
+++ lab_4_fill_words_by_ngrams/main.py
@@ -4,8 +4,8 @@ Lab 4.
 Top-p sampling generation and filling gaps with ngrams
 """
 # pylint:disable=too-few-public-methods, too-many-arguments
-import random
 import math
+import random
 
 from lab_3_generate_by_ngrams.main import (BeamSearchTextGenerator, GreedyTextGenerator,
                                            NGramLanguageModel, TextProcessor)
