e38d04113f843397a965d47d012d7324380f8e01
kobyakova polina <polinagg.pk@gmail.com>
2023-12-06 21:01:10 +0300

    lab4 score 6 fixing

--- lab_4_fill_words_by_ngrams/start.py
+++ lab_4_fill_words_by_ngrams/start.py
@@ -2,7 +2,8 @@
 Filling word by ngrams starter
 """
 # pylint:disable=too-many-locals,unused-import
-from lab_4_fill_words_by_ngrams.main import (WordProcessor, NGramLanguageModel, TopPGenerator)
+import lab_4_fill_words_by_ngrams.main as main_py
+#from  import WordProcessor, NGramLanguageModel, TopPGenerator
 
 
 def main() -> None:
@@ -13,11 +14,11 @@ def main() -> None:
         text = text_file.read()
     result = None
 
-    word_processor = WordProcessor('<eow>')
+    word_processor = main_py.WordProcessor('<eow>')
     encoded_text = word_processor.encode(text)
-    language_model = NGramLanguageModel(encoded_text, 2)
+    language_model = main_py.NGramLanguageModel(encoded_text, 2)
     language_model.build()
-    top_p_generator = TopPGenerator(language_model, word_processor, 0.5)
+    top_p_generator = main_py.TopPGenerator(language_model, word_processor, 0.5)
     result = top_p_generator.run(51, 'Vernon')
     print(result)
 
