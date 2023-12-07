922eb091b54240cc94fed29ebb1fda4220f7bb61
Соня <saonofrienko@edu.hse.ru>
2023-12-07 21:18:02 +0300

    some changes

--- lab_3_generate_by_ngrams/main.py
+++ lab_3_generate_by_ngrams/main.py
@@ -28,6 +28,7 @@ class TextProcessor:
         self._end_of_word_token = end_of_word_token
         self._storage = {end_of_word_token: 0}
 
+
     def _tokenize(self, text: str) -> Optional[tuple[str, ...]]:
         """
         Tokenize text into unigrams, separating words with special token.
