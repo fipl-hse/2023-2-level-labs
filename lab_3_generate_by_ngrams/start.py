01060bf30be0e16bdaec35b3686961aa63a1a18e
Baronofff <Baronov.ev@gmail.com>
2023-12-08 10:03:59 +0300

    Laboratory work #3, Evgeniy Baronov - 22FPL2

--- lab_3_generate_by_ngrams/start.py
+++ lab_3_generate_by_ngrams/start.py
@@ -13,18 +13,18 @@ def main() -> None:
     """
     with open("./assets/Harry_Potter.txt", "r", encoding="utf-8") as text_file:
         text = text_file.read()
-        processor = TextProcessor('_')
-        encoded = processor.encode(text)
-        result = processor.decode(encoded)
-        model = NGramLanguageModel(encoded[:100], 3)
-        print(model.build())
-        lang_model = NGramLanguageModel(encoded, 7)
-        print(lang_model.build())
-        greedy_text_generator = GreedyTextGenerator(lang_model, processor)
-        print(greedy_text_generator.run(51, 'Vernon'))
-        beam_search_generator = BeamSearchTextGenerator(lang_model, processor, 7)
-        print(beam_search_generator.run('Vernon', 56))
-        assert result
+    processor = TextProcessor('_')
+    encoded = processor.encode(text)
+    result = processor.decode(encoded)
+    model = NGramLanguageModel(encoded[:100], 3)
+    print(model.build())
+    lang_model = NGramLanguageModel(encoded, 7)
+    print(lang_model.build())
+    greedy_text_generator = GreedyTextGenerator(lang_model, processor)
+    print(greedy_text_generator.run(51, 'Vernon'))
+    beam_search_generator = BeamSearchTextGenerator(lang_model, processor, 7)
+    print(beam_search_generator.run('Vernon', 56))
+    assert result
 
 if __name__ == "__main__":
     main()
