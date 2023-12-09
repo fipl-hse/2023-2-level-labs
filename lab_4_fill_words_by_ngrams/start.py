75ce3b1d86db9673e0a78fbb3cfdda9ce1465135
AlisaKarpova <aliskakarpowa@yandex.ru>
2023-12-09 22:41:30 +0300

    start.py

--- lab_4_fill_words_by_ngrams/start.py
+++ lab_4_fill_words_by_ngrams/start.py
@@ -3,7 +3,7 @@ Filling word by ngrams starter
 """
 # pylint:disable=too-many-locals,unused-import
 
-from lab_3_generate_by_ngrams.main import BeamSearchTextGenerator, GreedyTextGenerator, NGramLanguageModel
+from lab_3_generate_by_ngrams.main import BeamSearchTextGenerator, NGramLanguageModel
 from lab_4_fill_words_by_ngrams.main import (GeneratorTypes, TopPGenerator,
                                              QualityChecker, WordProcessor)
 
