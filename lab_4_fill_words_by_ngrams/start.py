2773507e8dd6ccc7ca3a81a6d3b5640c54875975
Andrey Gusev <andrey.g9.ag@gmail.com>
2023-12-10 22:53:38 +0300

    fixed the mistakes

--- lab_4_fill_words_by_ngrams/start.py
+++ lab_4_fill_words_by_ngrams/start.py
@@ -2,8 +2,7 @@
 Filling word by ngrams starter
 """
 # pylint:disable=too-many-locals,unused-import
-from lab_3_generate_by_ngrams.main import NGramLanguageModel
-from lab_4_fill_words_by_ngrams.main import WordProcessor, TopPGenerator
+from lab_4_fill_words_by_ngrams.main import (NGramLanguageModel, TopPGenerator, WordProcessor)
 
 
 def main() -> None:
