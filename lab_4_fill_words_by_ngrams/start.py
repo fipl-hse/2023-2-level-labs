15bbb3e1e1c38cb5c69b788e55cb3b0dc7429213
tima-baranov <timobaranov@gmail.com>
2023-12-16 16:28:05 +0300

    changed start.py version

--- lab_4_fill_words_by_ngrams/start.py
+++ lab_4_fill_words_by_ngrams/start.py
@@ -2,8 +2,7 @@
 Filling word by ngrams starter
 """
 # pylint:disable=too-many-locals,unused-import
-from lab_3_generate_by_ngrams.main import NGramLanguageModel
-from lab_4_fill_words_by_ngrams.main import TopPGenerator, WordProcessor
+from lab_4_fill_words_by_ngrams.main import NGramLanguageModel, TopPGenerator, WordProcessor
 
 def main() -> None:
     """
@@ -15,8 +14,8 @@ def main() -> None:
     encoded_text = word_processor.encode(text)
     model = NGramLanguageModel(encoded_text, 2)
     model.build()
-    top_p = TopPGenerator(model, word_processor, 0.5)
-    result = top_p.run(51, 'Vernon')
+    generator = TopPGenerator(model, word_processor, 0.5)
+    result = generator.run(51, 'Vernon')
     print(result)
     assert result
 
