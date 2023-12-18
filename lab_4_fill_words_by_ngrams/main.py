f628f374a805006a14a1e4842289e292386d389c
Соня <saonofrienko@edu.hse.ru>
2023-12-18 15:54:00 +0300

    some changes

--- lab_4_fill_words_by_ngrams/main.py
+++ lab_4_fill_words_by_ngrams/main.py
@@ -5,10 +5,12 @@ Top-p sampling generation and filling gaps with ngrams
 """
 #pylint:disable=too-few-public-methods, too-many-arguments
 
-import json
 import math
+
 from random import choice
+
 from math import (exp, log)
+
 from lab_3_generate_by_ngrams.main import (BeamSearchTextGenerator, GreedyTextGenerator,
                                            NGramLanguageModel, TextProcessor)
 
