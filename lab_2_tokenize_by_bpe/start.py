1f9ca23369ad87c9ca95dc1ed9b86525f326f394
RostislavHmelevski <warian11031978@gmail.com>
2023-12-18 20:29:54 +0300

    Merge branch 'main' of https://github.com/RostislavHmelevski/2023-2-level-labs

--- lab_2_tokenize_by_bpe/start.py
+++ lab_2_tokenize_by_bpe/start.py
@@ -2,6 +2,7 @@
 BPE Tokenizer starter
 """
 from pathlib import Path
+from lab_2_tokenize_by_bpe.main import collect_frequencies
 
 
 def main() -> None:
@@ -11,9 +12,8 @@ def main() -> None:
     assets_path = Path(__file__).parent / 'assets'
     with open(assets_path / 'text.txt', 'r', encoding='utf-8') as text_file:
         text = text_file.read()
-
-    result = None
-    assert result, "Encoding is not working"
+    dictionary = collect_frequencies(text, None, '</s>')
+    print(dictionary)
 
 
 if __name__ == "__main__":
