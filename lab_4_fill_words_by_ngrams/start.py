4b624f8ae9a3a688a4f6153f4ff8d99475f41ca7
alesiamashkovtseva <alesiamashkovtseva@gmail.com>
2023-12-18 14:21:50 +0300

    fixing

--- lab_4_fill_words_by_ngrams/start.py
+++ lab_4_fill_words_by_ngrams/start.py
@@ -19,23 +19,24 @@ def main() -> None:
     lang_model = NGramLanguageModel(encoded_text, 2)
     lang_model.build()
     top_p_generator = TopPGenerator(lang_model, word_processor, 0.5)
-    top_p_result = top_p_generator.run(51, 'Vernon')
+
     generator_types = GeneratorTypes()
     generators = {generator_types.top_p: TopPGenerator(lang_model, word_processor, 0.5),
                   generator_types.beam_search: BeamSearchTextGenerator(lang_model,
                                                                        word_processor, 5)}
     checker = QualityChecker(generators, lang_model, word_processor)
-    # checker_result = checker.run(100, 'The')
-    examiner = Examiner('assets/questions_and_answers.json')
+
+    examiner = Examiner("/Users/alesamaskovceva/Documents/python/2023-2-level-labs/lab_4_fill_words_by_ngrams"
+                        "/assets/question_and_answers.json")
+    questions = examiner.provide_questions()
     students = [GeneratorRuleStudent(generator_types.greedy, lang_model, word_processor),
                 GeneratorRuleStudent(generator_types.top_p, lang_model, word_processor),
                 GeneratorRuleStudent(generator_types.beam_search, lang_model, word_processor)]
-    questions = examiner.provide_questions()
     answers = {student: student.take_exam(questions) for student in students}
     assessment = {student: examiner.assess_exam(answer) for student, answer in answers.items()}
     result = ""
     for student, accuracy in assessment.items():
-        result += f"Accuracy of student ({student.get_generator_type()}): {accuracy}\n"
+        result += f"{student.get_generator_type()}: {accuracy}\n"
     print(result)
 
     assert result
