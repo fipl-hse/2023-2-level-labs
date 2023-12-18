aa10ee212feadf0581f299e22acd29f558b4c4e8
mary <ma6ylabuk@gmail.com>
2023-12-18 12:49:20 +0300

    fixing..

--- lab_4_fill_words_by_ngrams/start.py
+++ lab_4_fill_words_by_ngrams/start.py
@@ -5,6 +5,7 @@ Filling word by ngrams starter
 # from lab_4_fill_words_by_ngrams.main import (NGramLanguageModel, TopPGenerator, WordProcessor)
 import lab_4_fill_words_by_ngrams.main as main_py
 
+
 def main() -> None:
     """
     Launches an implementation.
