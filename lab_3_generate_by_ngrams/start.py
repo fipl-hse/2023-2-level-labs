16a92d8c61697c604f40bc6e35a2e4a2ab541610
annakrapivtseva <annakrapivtseva@gmail.com>
2023-12-18 06:23:52 +0300

    check start

--- lab_3_generate_by_ngrams/start.py
+++ lab_3_generate_by_ngrams/start.py
@@ -3,6 +3,9 @@ Generation by NGrams starter
 """
 
 
+import lab_3_generate_by_ngrams.main as main_py
+
+
 def main() -> None:
     """
     Launches an implementation.
@@ -11,8 +14,18 @@ def main() -> None:
     """
     with open("./assets/Harry_Potter.txt", "r", encoding="utf-8") as text_file:
         text = text_file.read()
-    result = None
-    assert result
+        text_processor = main_py.TextProcessor('_')
+        encoded_text = text_processor.encode(text)
+        decoded_text = text_processor.decode(encoded_text)
+        language_model = main_py.NGramLanguageModel(encoded_text, 7)
+        greedy_generator = main_py.GreedyTextGenerator(language_model, text_processor)
+        generated_text = greedy_generator.run(51, 'Vernon')
+
+        beam_search_generator = main_py.BeamSearchTextGenerator(language_model, text_processor, 7)
+        resulted_text = beam_search_generator.run('Vernon', 56)
+
+        result = resulted_text
+        assert result
 
 
 if __name__ == "__main__":
