25ec3469cf15da9a64a04e04b94186e71a54cd1b
iriper <irinapereyas@gmail.com>
2023-12-18 04:59:47 +0300

    Laboratory work #4, Irina Pereyaslavtseva - 22FPL1

--- lab_4_fill_words_by_ngrams/main.py
+++ lab_4_fill_words_by_ngrams/main.py
@@ -5,6 +5,7 @@ Top-p sampling generation and filling gaps with ngrams
 """
 # pylint:disable=too-few-public-methods, too-many-arguments
 import random
+
 from lab_3_generate_by_ngrams.main import (BeamSearchTextGenerator, GreedyTextGenerator,
                                            NGramLanguageModel, TextProcessor)
 
